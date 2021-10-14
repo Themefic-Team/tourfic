39b755c6e52b801f414781003ee7a9834cd3466b
abuhena_bit <abuhena.office@gmail.com>
2021-10-14 18:37:04 +0600

    faq metabox added

--- admin/inc/tours/tf-tours-metabox.php
+++ admin/inc/tours/tf-tours-metabox.php
@@ -69,16 +69,16 @@ if ( class_exists( 'CSF' ) ) {
         'fields' => array(
 
             array(
-                'id'    => 'location',
-                'type'  => 'text',
-                'title' => __( 'Tour Location','tourfic' ),
-                'subtitle' => __( 'Select tour location','tourfic' ),
+                'id'       => 'location',
+                'type'     => 'text',
+                'title'    => __( 'Tour Location', 'tourfic' ),
+                'subtitle' => __( 'Select tour location', 'tourfic' ),
             ),
             array(
-                'id'    => 'nearby_properties',
-                'type'  => 'text',
-                'title' => __( 'Nearby properties','tourfic' ),
-                'subtitle' => __( 'Input nearby properties','tourfic' ),
+                'id'       => 'nearby_properties',
+                'type'     => 'text',
+                'title'    => __( 'Nearby properties', 'tourfic' ),
+                'subtitle' => __( 'Input nearby properties', 'tourfic' ),
             ),
 
         ),
@@ -90,28 +90,28 @@ if ( class_exists( 'CSF' ) ) {
         'fields' => array(
 
             array(
-                'id'    => 'email',
-                'type'  => 'text',
-                'title' => __( 'Email address','tourfic' ),
-                'subtitle' => __( 'Input email address','tourfic' ),
+                'id'       => 'email',
+                'type'     => 'text',
+                'title'    => __( 'Email address', 'tourfic' ),
+                'subtitle' => __( 'Input email address', 'tourfic' ),
             ),
             array(
-                'id'    => 'website',
-                'type'  => 'text',
-                'title' => __( 'Website Url','tourfic' ),
-                'subtitle' => __( 'Input website url','tourfic' ),
+                'id'       => 'website',
+                'type'     => 'text',
+                'title'    => __( 'Website Url', 'tourfic' ),
+                'subtitle' => __( 'Input website url', 'tourfic' ),
             ),
             array(
-                'id'    => 'phone',
-                'type'  => 'text',
-                'title' => __( 'Phone Number','tourfic' ),
-                'subtitle' => __( 'Input Phone Number','tourfic' ),
+                'id'       => 'phone',
+                'type'     => 'text',
+                'title'    => __( 'Phone Number', 'tourfic' ),
+                'subtitle' => __( 'Input Phone Number', 'tourfic' ),
             ),
             array(
-                'id'    => 'fax',
-                'type'  => 'text',
-                'title' => __( 'Fax Number','tourfic' ),
-                'subtitle' => __( 'Input Fax Number','tourfic' ),
+                'id'       => 'fax',
+                'type'     => 'text',
+                'title'    => __( 'Fax Number', 'tourfic' ),
+                'subtitle' => __( 'Input Fax Number', 'tourfic' ),
             ),
 
         ),
@@ -123,42 +123,89 @@ if ( class_exists( 'CSF' ) ) {
         'fields' => array(
 
             array(
-                'id'    => 'duration',
-                'type'  => 'text',
-                'title' => __( 'Tour duration','tourfic' ),
-                'subtitle' => __( 'Tour duration eg. 3 days','tourfic' ),
+                'id'       => 'duration',
+                'type'     => 'text',
+                'title'    => __( 'Tour duration', 'tourfic' ),
+                'subtitle' => __( 'Tour duration eg. 3 days', 'tourfic' ),
             ),
             array(
-                'id'    => 'min_days',
-                'type'  => 'slider',
-                'title' => __( 'Minimum days to book before departure','tourfic' ),
-                'subtitle' => __( 'Minimum days to book before departure','tourfic' ),
+                'id'       => 'min_days',
+                'type'     => 'slider',
+                'max'      => '30',
+                'title'    => __( 'Minimum days to book before departure', 'tourfic' ),
+                'subtitle' => __( 'Minimum days to book before departure', 'tourfic' ),
             ),
             array(
-                'id'    => 'external_booking',
-                'type'  => 'switcher',
-                'title' => __( 'Allow external booking','tourfic' ),
-                'subtitle' => __( 'Clik to allow external booking','tourfic' ),
+                'id'       => 'external_booking',
+                'type'     => 'switcher',
+                'title'    => __( 'Allow external booking', 'tourfic' ),
+                'subtitle' => __( 'Clik to allow external booking', 'tourfic' ),
             ),
             array(
-                'id'    => 'external_booking_link',
-                'type'  => 'text',
-                'title' => __( 'External booking link','tourfic' ),
-                'subtitle' => __( 'Input external booking link','tourfic' ),
+                'id'         => 'external_booking_link',
+                'type'       => 'text',
+                'title'      => __( 'External booking link', 'tourfic' ),
+                'subtitle'   => __( 'Input external booking link', 'tourfic' ),
+                'dependency' => array( 'external_booking', '==', 'true' ),
             ),
             array(
-                'id'    => 'min_people',
-                'type'  => 'text',
-                'title' => __( 'Minimum person','tourfic' ),
-                'subtitle' => __( 'Minimum person to travel','tourfic' ),
+                'id'       => 'min_people',
+                'type'     => 'text',
+                'title'    => __( 'Minimum person', 'tourfic' ),
+                'subtitle' => __( 'Minimum person to travel', 'tourfic' ),
             ),
             array(
-                'id'    => 'max_people',
-                'type'  => 'text',
-                'title' => __( 'Maximum person','tourfic' ),
-                'subtitle' => __( 'Maximum person to travel','tourfic' ),
+                'id'       => 'max_people',
+                'type'     => 'text',
+                'title'    => __( 'Maximum person', 'tourfic' ),
+                'subtitle' => __( 'Maximum person to travel', 'tourfic' ),
+            ),
+            array(
+                'id'       => 'date',
+                'type'     => 'date',
+                'title'    => __( 'Maximum person', 'tourfic' ),
+                'subtitle' => __( 'Maximum person to travel', 'tourfic' ),
             ),
 
+        ),
+    ) );
+
+    CSF::createSection( $prefix, array(
+        'title'  => __( 'Availability', 'tourfic' ),
+        'fields' => array(
+            array(
+                'id'    => 'check_in',
+                'type'  => 'date',
+                'title' => __( 'Check In', 'tourfic' ),
+            ),
+            array(
+                'id'    => 'check_out',
+                'type'  => 'date',
+                'title' => __( 'Check Out', 'tourfic' ),
+            ),
+        ),
+    ) );
+
+    CSF::createSection( $prefix, array(
+        'title'  => __( 'FAQs', 'tourfic' ),
+        'fields' => array(
+            array(
+                'id'     => 'faq',
+                'type'   => 'repeater',
+                'title'  => __( 'FAQs', 'tourfic' ),
+                'fields' => array(
+                    array(
+                        'id'    => 'title',
+                        'type'  => 'text',
+                        'title' => __( 'FAQ title', 'tourfic' ),
+                    ),
+                    array(
+                        'id'    => 'desc',
+                        'type'  => 'textarea',
+                        'title' => __( 'FAQ description', 'tourfic' ),
+                    ),
+                ),
+            ),
         ),
     ) );
 
