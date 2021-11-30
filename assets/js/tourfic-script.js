404ce021533a6fe9722821e019d93932a7e91efd
abuhena_bit <abuhena.office@gmail.com>
2021-11-30 17:28:02 +0600

    Search widget tab added for hotel and tours

--- assets/js/tourfic-script.js
+++ assets/js/tourfic-script.js
@@ -826,4 +826,21 @@ var destinations = tf_params.destinations;
 
 tourfic_autocomplete(document.getElementById("destination"), destinations);
 
+/**
+ * Searchbox widgets tab scripts
+ */
+ function tfOpenForm(evt, formName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tf-tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tf-tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(formName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+
 
