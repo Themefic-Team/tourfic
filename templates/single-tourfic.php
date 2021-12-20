d6e2e4f344b1549a4588943a41e0c7a5fde70599
abuhena_bit <abuhena.office@gmail.com>
2021-12-20 18:53:58 +0600

    issue fixed

--- templates/single-tourfic.php
+++ templates/single-tourfic.php
@@ -254,7 +254,6 @@ $tf_faqs = ( get_post_meta( $post->ID, 'tf_faqs', true ) ) ? get_post_meta( $pos
 				</div>
 				<!-- End TOC Content -->
 			</div>
-			</div>
 			<!-- End Content -->
 
 			<!-- Start Sidebar -->
@@ -262,6 +261,9 @@ $tf_faqs = ( get_post_meta( $post->ID, 'tf_faqs', true ) ) ? get_post_meta( $pos
 				<?php tourfic_get_sidebar( 'single' ); ?>
 			</div>
 			<!-- End Sidebar -->
+			
+		</div>
+		</div>
 		</div>
 	</div>
 	<?php do_action( 'tf_after_container' ); ?>
