8eaeeb82a3eb05b22bfe4fa5f55452d3673599df
jahid-dev <jahidhasancmt7@gmail.com>
2022-07-21 17:33:38 +0600

    documentation info add

--- inc/functions/functions-helper.php
+++ inc/functions/functions-helper.php
@@ -15,6 +15,42 @@ function tf_booking_search_action() {
 }
 
 /**
+ * Go to Documentaion Menu Item 
+ */
+
+add_action('admin_menu', 'tf_documentation_page_integration');
+function tf_documentation_page_integration() {
+	global $submenu;
+	$tfhoteldocumentation = sanitize_url('https://themefic.com/docs/tourfic/');
+	$tftourdocumentation = sanitize_url('https://themefic.com/docs/tourfic/');
+	$submenu['edit.php?post_type=tf_hotel'][] = array( '<span style=color:#ffba00;">Go to Documentation</span>', 'edit_tf_hotels', $tfhoteldocumentation );
+	$submenu['edit.php?post_type=tf_tours'][] = array( '<span style=color:#ffba00;">Go to Documentation</span>', 'edit_tf_tourss', $tftourdocumentation );
+}
+
+/**
+ * Go to Documentaion Metabox
+ */
+
+function tf_hotel_tour_docs() {
+    add_meta_box( 'tfhotel_docs', __( 'Tourfic Documantation', 'tourfic' ), 'tf_hotel_docs_callback','tf_hotel','side' ,'high');
+    add_meta_box( 'tftour_docs', __( 'Tourfic Documantation', 'tourfic' ), 'tf_tour_docs_callback','tf_tours','side' ,'high');
+}
+add_action( 'add_meta_boxes', 'tf_hotel_tour_docs' );
+
+function tf_hotel_docs_callback(){
+	$tfhoteldocumentation =sanitize_url('https://themefic.com/docs/tourfic/');
+?>
+	<a href="<?php echo $tfhoteldocumentation; ?>" target="_blank" class="button button-primary button-large"><?php echo __('Go to Documentation','tourfic'); ?></a>
+<?php
+}
+function tf_tour_docs_callback(){
+	$tftourdocumentation =sanitize_url('https://themefic.com/docs/tourfic/');
+?>
+	<a href="<?php echo $tftourdocumentation; ?>" target="_blank" class="button button-primary button-large"><?php echo __('Go to Documentation','tourfic'); ?></a>
+<?php
+}
+
+/**
  * Notice wrapper
  */
 function tourfic_notice_wrapper() {
