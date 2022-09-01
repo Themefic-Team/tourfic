cec131efd806a0be2c6a63516dced824e008abc8
Sydur Rahman <sydurrahmant1@gmail.com>
2022-09-01 18:32:30 +0600

    booking.com form add

--- admin/options/global/parts/general.php
+++ admin/options/global/parts/general.php
@@ -3,23 +3,23 @@
 defined( 'ABSPATH' ) || exit;
 
 CSF::createSection( $prefix, array(
-    'id'     => 'general',
-    'title'  => __( 'General', 'tourfic' ),
-    'icon'   => 'fas fa-cogs',
-    'fields' => array(
+	'id'     => 'general',
+	'title'  => __( 'General', 'tourfic' ),
+	'icon'   => 'fas fa-cogs',
+	'fields' => array(
 
-      array(
-        'id'       => 'disable-services',
-        'type'     => 'checkbox',
-        'title'    => __('Disable Services', 'tourfic' ),
-        'subtitle' => __('Disable or hide the services you don\'t need by ticking the checkbox', 'tourfic' ),
-        'options'  => array(
-            'hotel' => __('Hotel', 'tourfic' ),
-            'tour'  => __('Tour', 'tourfic' ),
-        ),
-        ),
+		array(
+			'id'       => 'disable-services',
+			'type'     => 'checkbox',
+			'title'    => __( 'Disable Services', 'tourfic' ),
+			'subtitle' => __( 'Disable or hide the services you don\'t need by ticking the checkbox', 'tourfic' ),
+			'options'  => array(
+				'hotel' => __( 'Hotel', 'tourfic' ),
+				'tour'  => __( 'Tour', 'tourfic' ),
+			),
+		),
 
-    )
-    
+	)
+
 ) );
 ?>
