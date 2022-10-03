d1c7282361d375536e28527a840b54af33ed71bd
jahid-dev <jahidhasancmt7@gmail.com>
2022-10-03 10:47:27 +0600

    custom feature

--- templates/common/search-results.php
+++ templates/common/search-results.php
@@ -2,18 +2,19 @@
 <div class="tf-main-wrapper search-result-wrapper" data-fullwidth="true">
     <?php do_action( 'tf_before_container' ); ?>
 	<div class="tf-container">
-        <div class="search-result-inner">
+        <div class="search-result-inner tf-custom-search-results">
+			<!-- Start Sidebar -->
+			<div class="tf-search-right">
+				<?php tf_search_result_sidebar_form( 'archive' ); ?>
+			</div>
+			<!-- End Sidebar -->
             <!-- Start Content -->           
 			<div class="tf-search-left">
 				<?php echo do_shortcode("[tf_search_result]"); ?>
 			</div>
 			<!-- End Content -->
 
-			<!-- Start Sidebar -->
-			<div class="tf-search-right">
-				<?php tf_search_result_sidebar_form( 'archive' ); ?>
-			</div>
-			<!-- End Sidebar -->
+			
 		</div>
 	</div>
 	<?php do_action( 'tf_after_container' ); ?>
