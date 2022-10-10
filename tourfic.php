05f1d74208563911a49c2a8659285c23cd6c5211
kamrul07 <kamrul07@users.noreply.github.com>
2022-10-10 15:20:46 +0600

    Update to 2.8.8

--- tourfic.php
+++ tourfic.php
@@ -7,7 +7,7 @@
  * Author URI:      https://themefic.com
  * Text Domain:     tourfic
  * Domain Path:     /lang/
- * Version:         2.8.7
+ * Version:         2.8.8
  * Tested up to: 6.0.2
  * WC tested up to: 6.9.4
  * Requires PHP: 7.2
