caae630ba771894367eeeb72c6cba49acb4d3061
jahid-dev <jahidhasancmt7@gmail.com>
2022-10-23 10:47:05 +0600

    str repleace issue fixed

--- admin/tf-options/fields/gallery/TF_gallery.php
+++ admin/tf-options/fields/gallery/TF_gallery.php
@@ -15,7 +15,7 @@ if ( ! class_exists( 'TF_gallery' ) ) {
 		public function render() {
 			$type = ( ! empty( $this->field['type'] ) ) ? $this->field['type'] : 'text';
 
-			echo '<div class="tf-fieldset-gallery-preview ' . str_replace(array("[","]"),"_",esc_attr( $this->field_name() ) ) . '">';
+			echo '<div class="tf-fieldset-gallery-preview ' . str_replace(array("[","]","-"),"_",esc_attr( $this->field_name() ) ) . '">';
 			if ( !empty( $this->value ) ) {
 				$tf_gallery_ids = explode( ',', $this->value );
 				foreach ( $tf_gallery_ids as $key => $gallery_item_id ) {
@@ -28,9 +28,9 @@ if ( ! class_exists( 'TF_gallery' ) ) {
 			<div class="tf-fieldset-gallery">
 			<a href="#" tf-field-name="' . esc_attr( $this->field_name() ) . '" class="tf-gallery-upload button button-primary button-large">' . esc_html( "Add Gallery","tourfic" ) . '</a>';
 			if(!empty($this->value)){
-				echo '<a href="#" tf-field-name="' . esc_attr( $this->field_name() ) . '" class="tf-gallery-remove button button-warning button-large ' . str_replace(array("[","]"),"_",esc_attr( $this->field_name() ) ) . '" style="display:inline-block">' . esc_html( "Clear","tourfic" ) . '</a>';
+				echo '<a href="#" tf-field-name="' . esc_attr( $this->field_name() ) . '" class="tf-gallery-remove button button-warning button-large ' . str_replace(array("[","]","-"),"_",esc_attr( $this->field_name() ) ) . '" style="display:inline-block">' . esc_html( "Clear","tourfic" ) . '</a>';
 			}else{
-				echo '<a href="#" tf-field-name="' . esc_attr( $this->field_name() ) . '" class="tf-gallery-remove button button-warning button-large ' . str_replace(array("[","]"),"_",esc_attr( $this->field_name() ) ) . '" style="display:none">' . esc_html( "Clear","tourfic" ) . '</a>';
+				echo '<a href="#" tf-field-name="' . esc_attr( $this->field_name() ) . '" class="tf-gallery-remove button button-warning button-large ' . str_replace(array("[","]","-"),"_",esc_attr( $this->field_name() ) ) . '" style="display:none">' . esc_html( "Clear","tourfic" ) . '</a>';
 			}
 			echo '</div>
 			<input type="hidden" name="' . esc_attr( $this->field_name() ) . '" id="' . esc_attr( $this->field_name() ) . '" value="' . $this->value . '"  />';
