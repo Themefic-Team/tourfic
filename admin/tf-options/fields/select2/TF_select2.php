cd9fe3f33cf3fb8807c8b7aa82461786b809b31a
unknown <sydurrahmant1@gmail.com>
2022-11-09 16:51:27 +0600

    select 2 repeater issue fixed

--- admin/tf-options/fields/select2/TF_select2.php
+++ admin/tf-options/fields/select2/TF_select2.php
@@ -44,8 +44,11 @@ if ( ! class_exists( 'TF_select2' ) ) {
 			}
 
 			$field_name = !empty($this->field['multiple']) ? $this->field_name() . '[]' : $this->field_name();
+			$tf_select2_unique_id = str_replace( array("[","]"),"_",esc_attr( $this->field_name() ) );
+			$parent_class = ( ! empty( $this->parent_field ) ) ? 'tf-select2-parent' : 'tf-select2'; 
+            $parent_class = ( isset( $this->field['select2'] ) ) ? 'tf-select2' : $parent_class ;  
 
-			echo '<select name="' . $field_name . '" id="' . esc_attr( $this->field_name() ) . '" class="tf-select2" data-placeholder="' . esc_attr( $placeholder ) . '" ' . $multiple . ' '. $this->field_attributes() .'>';
+			echo '<select name="' . $field_name . '" id="' . $tf_select2_unique_id . '" class=" tf-select-two '.$parent_class.' " data-placeholder="' . esc_attr( $placeholder ) . '" ' . $multiple . ' '. $this->field_attributes() .'>';
 			foreach ( $args['options'] as $key => $value ) {
 				if(!empty($this->field['multiple']) && is_array( $this->value ) && in_array( $key, $this->value )){
 					$selected = 'selected';
--- admin/tf-options/fields/switch/TF_switch.php
+++ admin/tf-options/fields/switch/TF_switch.php
@@ -20,8 +20,8 @@ if ( ! class_exists( 'TF_switch' ) ) {
 			$off = ( ! empty( $args['label_off'] ) ) ? $args['label_off'] : esc_html__( 'Off', 'tourfic' );
 			$width = ( ! empty( $this->field['width'] ) ) ? ' style="width: '. esc_attr( $this->field['width'] ) .'px;"': '';
 			?>
-            <label class="tf-switch-label" <?php echo wp_kses_post($width); ?>>
-                <input type="checkbox" name="<?php echo esc_attr( $this->field_name() ); ?>" value="<?php echo $this->value; ?>" data-depend-id="<?php echo esc_attr( $this->field['id'] ); ?><?php echo esc_attr($this->parent_field); ?>" class="tf-switch" <?php checked( $this->value, 1 ); ?> <?php echo $this->field_attributes() ?>/>
+            <label for="<?php echo esc_attr( $this->field_name() ); ?>" class="tf-switch-label" <?php echo wp_kses_post($width); ?>>
+                <input type="checkbox" id="<?php echo esc_attr( $this->field_name() ); ?>" name="<?php echo esc_attr( $this->field_name() ); ?>" value="<?php echo $this->value; ?>" data-depend-id="<?php echo esc_attr( $this->field['id'] ); ?><?php echo esc_attr($this->parent_field); ?>" class="tf-switch" <?php checked( $this->value, 1 ); ?> <?php echo $this->field_attributes() ?>/>
                 <span class="tf-switch-slider">
                     <span class="tf-switch-on"><?php echo esc_html( $on ); ?></span>
                     <span class="tf-switch-off"><?php echo esc_html( $off ); ?></span>
