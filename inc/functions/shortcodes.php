e6a100d8a81c6b2352b09d2c0d361d82cf913500
mehedifoysal <mehedifoysal8@gmail.com>
2022-12-07 18:14:04 +0600

    apartment locations shortcode

--- inc/functions/shortcodes.php
+++ inc/functions/shortcodes.php
@@ -139,6 +139,78 @@ function shortcode_tour_destinations( $atts, $content = null ) {
 add_shortcode( 'tour_destinations', 'shortcode_tour_destinations' );
 
 /**
+ * Apartment location shortcode
+ * shortcode: apartment_locations
+ *
+ * @author Foysal
+ */
+function shortcode_apartment_location( $atts, $content = null ) {
+
+	// Shortcode extract
+	extract(
+		shortcode_atts(
+			array(
+				'orderby'    => 'name',
+				'order'      => 'ASC',
+				'hide_empty' => 0,
+				'ids'        => '',
+				'limit'      => - 1,
+			),
+			$atts
+		)
+	);
+
+	$locations = get_terms( array(
+		'taxonomy'     => 'apartment_location',
+		'orderby'      => $orderby,
+		'order'        => $order,
+		'hide_empty'   => $hide_empty,
+		'hierarchical' => 0,
+		'search'       => '',
+		'number'       => $limit == - 1 ? false : $limit,
+		'include'      => $ids,
+	) );
+
+	shuffle( $locations );
+	ob_start();
+
+	if ( $locations ) { ?>
+        <section id="recomended_section_wrapper">
+            <div class="recomended_inner">
+
+				<?php foreach ( $locations as $term ) {
+
+					$meta      = get_term_meta( $term->term_id, 'tf_apartment_location', true );
+					$image_url = ! empty( $meta['image'] ) ? $meta['image'] : TF_ASSETS_URL . 'img/img-not-available.svg';
+					$term_link = get_term_link( $term );
+
+					if ( is_wp_error( $term_link ) ) {
+						continue;
+					} ?>
+
+                    <div class="single_recomended_item">
+                        <a href="<?php echo $term_link; ?>">
+                            <div class="single_recomended_content" style="background-image: url(<?php echo $image_url; ?>);">
+                                <div class="recomended_place_info_header">
+                                    <h3><?php echo esc_html( $term->name ); ?></h3>
+                                    <p><?php printf( _n( '%s apartment', '%s apartments', $term->count, 'tourfic' ), $term->count ); ?></p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+
+				<?php } ?>
+
+            </div>
+        </section>
+	<?php }
+
+	return ob_get_clean();
+}
+
+add_shortcode( 'apartment_locations', 'shortcode_apartment_location' );
+
+/**
  * Recent Hotel Slider
  */
 function tf_recent_hotel_shortcode( $atts, $content = null ) {
