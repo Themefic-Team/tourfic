0819994360e5a8740e5c4a3227c5fc2886882b04
mehedifoysal <mehedifoysal8@gmail.com>
2022-12-08 10:39:27 +0600

    recent apartment shortcode

--- inc/functions/shortcodes.php
+++ inc/functions/shortcodes.php
@@ -140,7 +140,7 @@ add_shortcode( 'tour_destinations', 'shortcode_tour_destinations' );
 
 /**
  * Apartment location shortcode
- * shortcode: apartment_locations
+ * shortcode: [apartment_locations]
  *
  * @author Foysal
  */
@@ -260,7 +260,7 @@ function tf_recent_hotel_shortcode( $atts, $content = null ) {
 					$hotel_loop->the_post();
 					$post_id                = get_the_ID();
 					$related_comments_hotel = get_comments( array( 'post_id' => $post_id ) );
-					$meta                   = get_post_meta( $post_id, 'tf_hotel', true );
+					$meta                   = get_post_meta( $post_id, 'tf_hotels_opt', true );
 					$rooms                  = ! empty( $meta['room'] ) ? $meta['room'] : '';
 					if ( ! empty( $rooms ) && gettype( $rooms ) == "string" ) {
 						$tf_hotel_rooms_value = preg_replace_callback( '!s:(\d+):"(.*?)";!', function ( $match ) {
@@ -395,6 +395,88 @@ function tf_recent_tour_shortcode( $atts, $content = null ) {
 add_shortcode( 'tf_recent_tour', 'tf_recent_tour_shortcode' );
 // Old
 add_shortcode( 'tf_tours_grid', 'tf_recent_tour_shortcode' );
+
+/**
+ * Recent Apartment Slider
+ * shortcode: [tf_recent_apartment]
+ *
+ * @author Foysal
+ */
+function tf_recent_apartment_shortcode( $atts, $content = null ) {
+	extract(
+		shortcode_atts(
+			array(
+				'title'        => '',
+				'subtitle'     => '',
+				'count'        => 10,
+				'slidestoshow' => 5,
+			),
+			$atts
+		)
+	);
+
+	$args = array(
+		'post_type'      => 'tf_apartment',
+		'post_status'    => 'publish',
+		'orderby'        => 'date',
+		'order'          => 'DESC',
+		'posts_per_page' => $count,
+	);
+
+	ob_start();
+
+	$apartment_loop = new WP_Query( $args );
+
+	// Generate an Unique ID
+	$thisid = uniqid( 'tfpopular_' );
+
+	?>
+	<?php if ( $apartment_loop->have_posts() ) : ?>
+        <div class="tf-widget-slider recent-hotel-slider">
+            <div class="tf-heading">
+				<?php
+				if ( ! empty( $title ) ) {
+					echo '<h2>' . esc_html( $title ) . '</h2>';
+				}
+				if ( ! empty( $subtitle ) ) {
+					echo '<p>' . esc_html( $subtitle ) . '</p>';
+				}
+				?>
+            </div>
+
+            <div class="tf-slider-items-wrapper">
+				<?php while ( $apartment_loop->have_posts() ) {
+					$apartment_loop->the_post();
+					$post_id                = get_the_ID();
+					$related_comments_apartment = get_comments( array( 'post_id' => $post_id ) );
+					$meta                   = get_post_meta( $post_id, 'tf_apartment_opt', true );
+
+					?>
+                    <div class="tf-slider-item" style="background-image: url(<?php echo get_the_post_thumbnail_url( $post_id, 'full' ); ?>);">
+                        <div class="tf-slider-content">
+                            <div class="tf-slider-desc">
+                                <h3>
+                                    <a href="<?php the_permalink() ?>"><?php the_title() ?></a>
+                                </h3>
+								<?php if ( $related_comments_apartment ) { ?>
+                                    <div class="tf-slider-rating-star">
+                                        <i class="fas fa-star"></i> <span style="color:#fff;"><?php echo tf_total_avg_rating( $related_comments_apartment ); ?></span>
+                                    </div>
+								<?php } ?>
+                                <p><?php echo wp_trim_words( get_the_content(), 10 ); ?></p>
+                            </div>
+                        </div>
+                    </div>
+				<?php } ?>
+            </div>
+        </div>
+	<?php endif;
+	wp_reset_postdata(); ?>
+
+	<?php return ob_get_clean();
+}
+
+add_shortcode( 'tf_recent_apartment', 'tf_recent_apartment_shortcode' );
 
 /**
  * Search form
