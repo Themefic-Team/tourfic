23bd4f6594e7d423b8029c95d952bc648ddd4629
unknown <sydurrahmant1@gmail.com>
2023-01-18 11:07:31 +0600

    review admin notice feature aded

--- inc/functions.php
+++ inc/functions.php
@@ -1500,3 +1500,114 @@ function tf_assign_taxonomies( $post_id, $post, $old_status ){
 	
 }
 
+/**
+ * Review Admin Notice Feature
+ * @author Sydur Rahman
+ * @since 2.9.3
+ */
+
+// Themefic Plugin Set Admin Notice Status
+if(!function_exists('tf_review_activation_status')){
+
+    function tf_review_activation_status(){ 
+        $tf_installation_date = get_option('tf_installation_date'); 
+        if( !isset($_COOKIE['tf_installation_date']) && empty($tf_installation_date) && $tf_installation_date == 0){
+            setcookie('tf_installation_date', 1, time() + (86400 * 7), "/"); 
+        }else{
+            update_option( 'tf_installation_date', '1' );
+        }
+    }
+    add_action('admin_init', 'tf_review_activation_status');
+}
+
+// Themefic Plugin Review Admin Notice
+if(!function_exists('tf_review_notice')){
+    
+     function tf_review_notice(){ 
+        $get_current_screen = get_current_screen();  
+        if($get_current_screen->base == 'dashboard'){
+            $current_user = wp_get_current_user(); 
+        ?>
+            <div class="notice notice-info themefic_review_notice"> 
+               
+                <?php echo sprintf( 
+                        __( ' <p>Hey %1$s 👋, You have been using <b>%2$s</b> for quite a while. If you feel %2$s is helping your business to grow in any way, would you please help %2$s to grow by simply leaving a 5* review on the WordPress Forum?', 'tourfic' ),
+                        $current_user->display_name,
+                        'Tourfic'
+                    ); ?> 
+                
+                <ul>
+                    <li><a target="_blank" href="<?php echo esc_url('https://wordpress.org/support/plugin/tourfic/reviews/#new-post') ?>" class=""><span class="dashicons dashicons-external"></span><?php _e(' Ok, you deserve it!', 'tourfic' ) ?></a></li>
+                    <li><a href="#" class="already_done" data-status="already"><span class="dashicons dashicons-smiley"></span> <?php _e('I already did', 'tourfic') ?></a></li>
+                    <li><a href="#" class="later" data-status="later"><span class="dashicons dashicons-calendar-alt"></span> <?php _e('Maybe Later', 'tourfic') ?></a></li>
+                    <li><a target="_blank"  href="<?php echo esc_url('https://themefic.com/docs/tourfic/') ?>" class=""><span class="dashicons dashicons-sos"></span> <?php _e('I need help', 'tourfic') ?></a></li>
+                    <li><a href="#" class="never" data-status="never"><span class="dashicons dashicons-dismiss"></span><?php _e('Never show again', 'tourfic') ?> </a></li> 
+                </ul>
+                <button type="button" class="notice-dismiss review_notice_dismiss"><span class="screen-reader-text">Dismiss this notice.</span></button>
+            </div>
+
+            <!--   Themefic Plugin Review Admin Notice Script -->
+            <script>
+                jQuery(document).ready(function($) {
+                    $(document).on('click', '.already_done, .later, .never', function( event ) {
+                        event.preventDefault();
+                        var $this = $(this);
+                        var status = $this.attr('data-status'); 
+                        $this.closest('.themefic_review_notice').css('display', 'none')
+                        data = {
+                            action : 'tf_review_notice_callback',
+                            status : status,
+                        };
+
+                        $.ajax({
+                            url: ajaxurl,
+                            type: 'post',
+                            data: data,
+                            success: function (data) { ;
+                            },
+                            error: function (data) { 
+                            }
+                        });
+                    });
+
+                    $(document).on('click', '.review_notice_dismiss', function( event ) {
+                        event.preventDefault(); 
+						var $this = $(this);
+                        $this.closest('.themefic_review_notice').css('display', 'none')
+                        
+                    });
+                });
+
+            </script>
+        <?php  
+        }
+     }
+     $tf_review_notice_status = get_option('tf_review_notice_status'); 
+     $tf_installation_date = get_option('tf_installation_date'); 
+     if(isset($tf_review_notice_status) && $tf_review_notice_status <= 0 && $tf_installation_date == 1 && !isset($_COOKIE['tf_review_notice_status']) && !isset($_COOKIE['tf_installation_date'])){ 
+        add_action( 'admin_notices', 'tf_review_notice' );  
+     }
+     
+}
+
+ 
+// Themefic Plugin Review Admin Notice Ajax Callback 
+if(!function_exists('tf_review_notice_callback')){
+
+    function tf_review_notice_callback(){
+        $status = $_POST['status'];
+        if( $status == 'already'){ 
+            update_option( 'tf_review_notice_status', '1' );
+        }else if($status == 'never'){ 
+            update_option( 'tf_review_notice_status', '2' );
+        }else if($status == 'later'){
+            $cookie_name = "tf_review_notice_status";
+            $cookie_value = "1";
+            setcookie($cookie_name, $cookie_value, time() + (86400 * 7), "/"); 
+            update_option( 'tf_review_notice_status', '0' ); 
+        }  
+        wp_die();
+    }
+    add_action( 'wp_ajax_tf_review_notice_callback', 'tf_review_notice_callback' );
+
+}
