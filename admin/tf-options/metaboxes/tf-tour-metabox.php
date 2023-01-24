73c97e5245b795eb29adaebca1875a09ae0bca68
jahid-dev <jahidhasancmt7@gmail.com>
2023-01-24 17:52:21 +0600

    itinerary bug fixed

--- admin/tf-options/metaboxes/tf-tour-metabox.php
+++ admin/tf-options/metaboxes/tf-tour-metabox.php
@@ -2,6 +2,23 @@
 // don't load directly
 defined( 'ABSPATH' ) || exit;
 
+/**
+ * Get all the meals from glabal settings
+ * @author AbuHena
+ * @since 1.7.0
+ */
+function tf_tour_meals(){
+	$itinerary_options = ! empty( tf_data_types ( tfopt( 'itinerary-builder-setings' ) ) ) ? tf_data_types( tfopt( 'itinerary-builder-setings' ) ) : '';
+	$all_meals = [];
+	if( !empty( $itinerary_options['meals'] ) && is_array( $itinerary_options['meals'] ) ){
+		$meals = $itinerary_options['meals'];	
+		foreach ( $meals as $key => $meal ){
+			$all_meals[ $meal['meal'].$key ] = $meal['meal'];
+		}
+	}
+	return $all_meals;
+}
+
 TF_Metabox::metabox( 'tf_tours_opt', array(
 	'title'     => __( 'Tour Setting', 'tourfic' ),
 	'post_type' => 'tf_tours',
