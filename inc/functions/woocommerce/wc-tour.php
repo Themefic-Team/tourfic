2f2fdb7f248a151f569ca69bfa1d45109ba51c44
jahid-dev <jahidhasancmt7@gmail.com>
2023-01-30 11:38:39 +0600

    feedback fixing

--- inc/functions/woocommerce/wc-tour.php
+++ inc/functions/woocommerce/wc-tour.php
@@ -336,14 +336,14 @@ function tf_tours_booking_function() {
 			$adult_price    = floatval(preg_replace('/[^\d.]/', '', number_format( $adult_price - ( ( $adult_price / 100 ) * $discounted_price ), 2 )));
 			$children_price = floatval(preg_replace('/[^\d.]/', '', number_format( $children_price - ( ( $children_price / 100 ) * $discounted_price ), 2 )));
 			$infant_price   = floatval(preg_replace('/[^\d.]/', '', number_format( $infant_price - ( ( $infant_price / 100 ) * $discounted_price ), 2 )));
-			$group_price    = number_format( $group_price - ( ( $group_price / 100 ) * $discounted_price ), 2 );
+			$group_price    = floatval(preg_replace('/[^\d.]/', '', number_format( $group_price - ( ( $group_price / 100 ) * $discounted_price ), 2 )));
 
 		} elseif ( $discount_type == 'fixed' ) {
 
 			$adult_price    = floatval(preg_replace('/[^\d.]/', '', number_format( ( $adult_price - $discounted_price ), 2 )));
 			$children_price = floatval(preg_replace('/[^\d.]/', '', number_format( ( $children_price - $discounted_price ), 2 )));
 			$infant_price   = floatval(preg_replace('/[^\d.]/', '', number_format( ( $infant_price - $discounted_price ), 2 )));
-			$group_price    = number_format( ( $group_price - $discounted_price ), 2 );
+			$group_price    = floatval(preg_replace('/[^\d.]/', '', number_format( ( $group_price - $discounted_price ), 2 )));
 
 		}
 
