e96f6c7a171e07dd4360237fdd2851cd1255f979
jahid-dev <jahidhasancmt7@gmail.com>
2023-02-06 12:05:21 +0600

    vendor-assets-revamp

--- webpack-config.js
+++ webpack-config.js
@@ -5,6 +5,7 @@ const entryPoints = {};
 const freeAppJs = glob.sync('./sass/app/js/free/*.js');
 const freeAdminJs = glob.sync('./sass/admin/js/free/*.js');
 const addonAdminJs = glob.sync('./sass/admin/js/addon/*.js');
+const AddonAppJs = glob.sync('./sass/app/js/addon/*.js');
 const proAppJs = glob.sync('./sass/app/js/pro/*.js');
 const proAdminJs = ['./sass/admin/js/pro/locationpicker.jquery.js','./sass/admin/js/pro/locationpicker-custom.js','./sass/admin/js/pro/admin.js'];
 
@@ -18,6 +19,7 @@ entryPoints['tourfic-pro/assets/admin/js/tourfic-pro-admin'] = proAdminJs;
 
 //tourfic addon
 entryPoints['tourfic-vendor/admin/assets/js/tourfic-vendor-scripts.min'] = addonAdminJs;
+entryPoints['tourfic-vendor/public/assets/js/tourfic-vendor'] = AddonAppJs;
 
 //scss entry points
 // const appScss = glob.sync('./sass/app/css/tourfic.scss');
