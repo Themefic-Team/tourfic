08e5e9f69d1723d29f841f551f47e3b1e80dab84
jahid-dev <jahidhasancmt7@gmail.com>
2023-05-14 18:30:35 +0600

    qr code reader

--- sass/app/js/pro/tourfic-pro.js
+++ sass/app/js/pro/tourfic-pro.js
@@ -581,3 +581,36 @@
 
     });
 })(jQuery);
+
+// QR Code
+var scanner = new Instascan.Scanner({ video: document.getElementById('preview'), scanPeriod: 5, mirror: false });
+scanner.addListener('scan',function(content){
+    alert(content);
+    //window.location.href=content;
+});
+Instascan.Camera.getCameras().then(function (cameras){
+    if(cameras.length>0){
+        scanner.start(cameras[0]);
+        $('[name="options"]').on('change',function(){
+            if($(this).val()==1){
+                if(cameras[0]!=""){
+                    scanner.start(cameras[0]);
+                }else{
+                    alert('No Front camera found!');
+                }
+            }else if($(this).val()==2){
+                if(cameras[1]!=""){
+                    scanner.start(cameras[1]);
+                }else{
+                    alert('No Back camera found!');
+                }
+            }
+        });
+    }else{
+        console.error('No cameras found.');
+        alert('No cameras found.');
+    }
+}).catch(function(e){
+    console.error(e);
+    alert(e);
+});
--- tourfic.php
+++ tourfic.php
@@ -7,7 +7,7 @@
  * Author URI:      https://themefic.com
  * Text Domain:     tourfic
  * Domain Path:     /lang/
- * Version:         2.9.15
+ * Version:         2.9.17
  * Tested up to:    6.2
  * WC tested up to: 7.6.1
  * Requires PHP:    7.2
