6277d0336748b67bfe06c71b131db3cdbc241bbe
Ashik384 <39790492+Ashik384@users.noreply.github.com>
2023-06-07 17:33:58 +0600

    = 2.9.19 -  07 June 2023 =
    * Added: Booking details export ( Pro )
    * Added: Enquiry details export ( Pro )
    * Added: Pre Build Email template set to default ( Pro )
    * Added: Tour QR Code Integration (Pro)
    * Fixed: Archive page pagination issue (Free)
    * Fixed: Email sending issue fixed ( Free and Pro )

--- tourfic.php
+++ tourfic.php
@@ -7,11 +7,11 @@
  * Author URI:      https://themefic.com
  * Text Domain:     tourfic
  * Domain Path:     /lang/
- * Version:         2.9.18
+ * Version:         2.9.19
  * Tested up to:    6.2.2
- * WC tested up to: 7.7.0
+ * WC tested up to: 7.7.2
  * Requires PHP:    7.2
- * Elementor tested up to: 3.13.3
+ * Elementor tested up to: 3.13.4
  */
 
 // don't load directly
@@ -59,7 +59,7 @@ if(!class_exists('Appsero\Client')){
  * @since 1.0
  */
 if ( ! defined( 'TOURFIC' ) ) {
-	define( 'TOURFIC', '2.9.18' );
+	define( 'TOURFIC', '2.9.19' );
 }
 
 /**
