32bab49bb9e9210befe59421719fed5ec082752f
mehedifoysal <mehedifoysal8@gmail.com>
2023-06-14 15:07:12 +0600

    vendor assets update

--- sass/admin/js/addon/tourfic-vendor.js
+++ sass/admin/js/addon/tourfic-vendor.js
@@ -1,4 +1,3 @@
-
 (function ($) {
     $(document).ready(function () {
 
@@ -10,7 +9,7 @@
         $('.vendor-actions input#submit').click(function () {
             var balkaction = $('#tf_vendor_bulk').find(":selected").val();
             var vendorlist = $('input[name="vendor_id"]:checked').serializeArray();
-            if (balkaction !== "" && vendorlist.length > 0){
+            if (balkaction !== "" && vendorlist.length > 0) {
                 $("#tf-report-loader").addClass('show');
                 jQuery.ajax({
                     type: 'post',
@@ -29,46 +28,29 @@
         });
 
         /*
-        * Author @Jahid
+        * Author @Jahid, @Foysal
         * Vendor Status
         */
-
-        $('.vendor-status-switcher').click(function(){
-            if ($(this).is(':checked')) {
-                var vendorid= $(this).val();
-                $("#tf-report-loader").addClass('show');
-                jQuery.ajax({
-                    type: 'post',
-                    url: tf_vendor_params.ajax_url,
-                    data: {
-                        action: 'tf_vendor_activation',
-                        status: "enabled",
-                        vendorid: vendorid,
-                    },
-                    success: function (data) {
-                        $("#tf-report-loader").removeClass('show');
-                    }
-                });
-            }else{
-                var vendorid= $(this).val();
-                $("#tf-report-loader").addClass('show');
-                jQuery.ajax({
-                    type: 'post',
-                    url: tf_vendor_params.ajax_url,
-                    data: {
-                        action: 'tf_vendor_activation',
-                        status: "disabled",
-                        vendorid: vendorid,
-                    },
-                    success: function (data) {
-                        $("#tf-report-loader").removeClass('show');
-                    }
-                });
-            }
+        $('.vendor-status-switcher').click(function () {
+            let status = $(this).is(':checked') ? 'enabled' : 'disabled';
+            var vendorid = $(this).val();
+            $("#tf-report-loader").addClass('show');
+            jQuery.ajax({
+                type: 'post',
+                url: tf_vendor_params.ajax_url,
+                data: {
+                    action: 'tf_vendor_activation',
+                    status: status,
+                    vendorid: vendorid,
+                },
+                success: function (data) {
+                    $("#tf-report-loader").removeClass('show');
+                }
+            });
         });
 
         /*
-        * Author @Jahid
+        * Author @Jahid, @Foysal
         * Admin Vendor Registration
         */
         $(document).on('click', '#tf-vendor-register .tf-save-user', function (e) {
@@ -165,9 +147,6 @@
                 beforeSend: function (response) {
                     //alert(response);
                 },
-                complete: function (response) {
-                    //alert(response);
-                },
                 success: function (response) {
                     $("#tf-report-loader").removeClass('show');
                     $(".tf-vendor-reg-response").html(response);
@@ -185,13 +164,13 @@
         */
         $(document).on('click', '.tf-payout-pending', function (e) {
             e.preventDefault();
-            var withdraw_id =$(this).attr("data-id");
+            var withdraw_id = $(this).attr("data-id");
             var data = {
                 action: 'tf_vendor_payouts',
                 withdraw_id: withdraw_id
             };
             var admin_confirmation = confirm("Are you sure to Approved?");
-            if(admin_confirmation){
+            if (admin_confirmation) {
                 $("#tf-report-loader").addClass('show');
                 $.ajax({
                     type: 'post',
@@ -220,13 +199,13 @@
         */
         $(document).on('click', '.tf-payout-decline', function (e) {
             e.preventDefault();
-            var withdraw_id =$(this).attr("data-id");
+            var withdraw_id = $(this).attr("data-id");
             var data = {
                 action: 'tf_vendor_payouts_decline',
                 withdraw_id: withdraw_id
             };
             var admin_confirmation = confirm("Are you sure to Decline?");
-            if(admin_confirmation){
+            if (admin_confirmation) {
                 $("#tf-report-loader").addClass('show');
                 $.ajax({
                     type: 'post',
@@ -250,33 +229,85 @@
         });
 
         /*
-        * Author @Jahid
-        * Admin Vendor Image Upload
+        * vendor cover pic
+        * @author Foysal
         */
-
-        $('body').on('click', '.tf-update-vendor-img', function (e) {
-            frame = wp.media({
-                title: "Select Image",
-                button: {
-                    text: "Insert Image"
-                },
-                multiple: false
-            });
-            frame.on('select', function () {
-                var attachment = frame.state().get('selection').first().toJSON();
-                $('.tf_vendor_image').val(attachment.url);
-                $('.profile-pic').attr('src', attachment.url);
-            });
-            frame.open();
-            return false;
+        $('body').on('click', '.tf-cover-pic-edit', function (e) {
+            openMediaUploader('tf_cover_photo', 'full', '.tf-cover-pic');
         });
+        $('body').on('click', '.tf-cover-pic-delete', function (e) {
+            removeImage('tf_cover_photo', '.tf-cover-pic');
+        });
+
+        /*
+        * vendor profile pic
+        * @author Foysal
+        */
+        $('body').on('click', '.tf-profile-pic-edit', function (e) {
+            openMediaUploader('tf_user_image', 'thumbnail', '.tf-profile-image');
+        });
+        $('body').on('click', '.tf-profile-pic-delete', function (e) {
+            removeImage('tf_user_image', '.tf-profile-image');
+        });
+
+        /*
+        * Media Uploader
+        * @author Foysal
+        */
+        const openMediaUploader = (name, size, selector) => {
+
+            if (typeof wp !== 'undefined' && wp.media) {
+                const mediaFrame = wp.media({
+                    title: 'Select an image',
+                    button: {
+                        text: 'Use this image',
+                    },
+                    multiple: false,
+                });
+
+                mediaFrame.on('open', () => {
+                    if (mediaFrame.content.mode() !== 'browse') {
+                        mediaFrame.content.mode('browse'); // set the mode to browse (library tab)
+                    }
+                });
+
+                mediaFrame.on('select', () => {
+                    let attachmentUrl;
+                    const selected = mediaFrame.state().get('selection').first().toJSON();
+                    if (size) {
+                        const attachment = mediaFrame.state().get('selection').first().toJSON();
+                        attachmentUrl = attachment.sizes[size] ? attachment.sizes[size].url : attachment.url;
+                    } else {
+                        attachmentUrl = selected.url;
+                    }
+
+                    $(selector).attr('src', attachmentUrl);
+                    $(name).val(attachmentUrl);
+
+                });
+
+                mediaFrame.open();
+            } else {
+                console.log('wp.media is not available');
+            }
+        }
+
+        /*
+        * Media Remove
+        * @author Foysal
+        */
+        const removeImage = (name, selector) => {
+            let defaultVal = $(selector).data('default');
+            $(name).val('');
+            $(selector).attr('src', defaultVal);
+        }
 
         /*
         * Author @Jahid
         * Vendor Dashboard Chart
         */
 
-        if(tf_vendor_params.tf_vendor_chart_enable==1){       
+        if (tf_vendor_params.tf_vendor_chart_enable == 1) {
             var ctx = document.getElementById('tf_month_vendor_info'); // node
             var ctx = document.getElementById('tf_month_vendor_info').getContext('2d'); // 2d context
             var ctx = $('#tf_month_vendor_info'); // jQuery instance
@@ -288,24 +319,24 @@
                     labels: tf_vendor_params.tf_current_months_days,
                     // Information about the dataset
                     datasets: [{
-                            label : "Total Sales",
-                            borderColor: '#003C79',
-                            tension: 0.1,
-                            data: tf_vendor_params.tf_total_amount,       
-                            fill: false
-                        },
+                        label: "Total Sales",
+                        borderColor: '#003C79',
+                        tension: 0.1,
+                        data: tf_vendor_params.tf_total_amount,
+                        fill: false
+                    },
                         {
-                            label : "Vendor Amount",
+                            label: "Vendor Amount",
                             borderColor: 'red',
                             tension: 0.1,
-                            data: tf_vendor_params.tf_total_vendor_amount,       
+                            data: tf_vendor_params.tf_total_vendor_amount,
                             fill: false
                         },
                         {
-                            label : "Admin Commission",
+                            label: "Admin Commission",
                             borderColor: '#73A724',
                             tension: 0.1,
-                            data: tf_vendor_params.tf_total_commission, 
+                            data: tf_vendor_params.tf_total_commission,
                             fill: false,
                         }
                     ]
@@ -313,9 +344,9 @@
 
                 // Configuration options
                 options: {
-                layout: {
-                padding: 10,
-                },
+                    layout: {
+                        padding: 10,
+                    },
                     legend: {
                         display: true
                     },
@@ -336,15 +367,15 @@
         $(document).on('click', '.tf-vendor-submission', function () {
             var month = $(".tf-month").val();
             var year = $(".tf-year").val();
-            var chartval=[];
-            $.each($("input[name='tf-chart-report']:checked"), function(){            
+            var chartval = [];
+            $.each($("input[name='tf-chart-report']:checked"), function () {
                 chartval.push($(this).val());
             });
-            if(chartval.length == 0 || !month.length){
+            if (chartval.length == 0 || !month.length) {
                 alert("Both Fields are Required !");
                 return;
             }
-            if(chartval.length > 0){
+            if (chartval.length > 0) {
                 $("#tf-report-loader").addClass('show');
                 $('.tf-vendor-order-cart').find('iframe').remove();
             }
@@ -370,24 +401,24 @@
                             labels: response.tf_current_months_days,
                             // Information about the dataset
                             datasets: [{
-                                    label : response.tf_total_amount ? "Total Sales" : '',
-                                    borderColor: '#003C79',
-                                    tension: 0.1,
-                                    data: response.tf_total_amount,       
-                                    fill: false
-                                },
+                                label: response.tf_total_amount ? "Total Sales" : '',
+                                borderColor: '#003C79',
+                                tension: 0.1,
+                                data: response.tf_total_amount,
+                                fill: false
+                            },
                                 {
-                                    label : response.tf_total_vendor_amount ? "Vendor Amount" : '',
+                                    label: response.tf_total_vendor_amount ? "Vendor Amount" : '',
                                     borderColor: 'red',
                                     tension: 0.1,
-                                    data: response.tf_total_vendor_amount,       
+                                    data: response.tf_total_vendor_amount,
                                     fill: false
                                 },
                                 {
-                                    label : response.tf_total_commission ? "Admin Commission" : '',
+                                    label: response.tf_total_commission ? "Admin Commission" : '',
                                     borderColor: '#73A724',
                                     tension: 0.1,
-                                    data: response.tf_total_commission, 
+                                    data: response.tf_total_commission,
                                     fill: false
                                 }
                             ]
@@ -395,19 +426,19 @@
 
                         // Configuration options
                         options: {
-                        layout: {
-                        padding: 10,
-                        },
+                            layout: {
+                                padding: 10,
+                            },
                             legend: {
                                 labels: {
-                                    filter: function(legendItem, chartData) {
-                                        if (legendItem.datasetIndex === 0 ) {
+                                    filter: function (legendItem, chartData) {
+                                        if (legendItem.datasetIndex === 0) {
                                             return response.tf_total_amount ? true : false;
                                         }
-                                        if (legendItem.datasetIndex === 1 ) {
+                                        if (legendItem.datasetIndex === 1) {
                                             return response.tf_total_vendor_amount ? true : false;
                                         }
-                                        if (legendItem.datasetIndex === 2 ) {
+                                        if (legendItem.datasetIndex === 2) {
                                             return response.tf_total_commission ? true : false;
                                         }
                                     }
@@ -450,7 +481,7 @@
 
         $(document).on('change', '.tf-vendor-unique-id', function () {
             var vendor_id = $(this).val();
- 
+
             $("#tf-report-loader").addClass('show');
             jQuery.ajax({
                 type: 'post',
@@ -463,9 +494,9 @@
                     var response = JSON.parse(data);
                     // console.log(response.vendor_amount);
                     $(".tf-unique-vendor-amount").val(response.vendor_amount);
-                    if(response.vendor_amount==0){
+                    if (response.vendor_amount == 0) {
                         $(".tf-payout-submit-button").hide();
-                    }else{
+                    } else {
                         $(".tf-payout-submit-button").show();
                     }
                     $("#tf-report-loader").removeClass('show');
@@ -480,14 +511,14 @@
             var payment_release_date = $("#tf-payment-release-date").val();
             var payment_method = $("#tf-vendor-payment-method").val();
             var payment_note = $("#tf-payment-note").val();
-           
-            if( !vendor.length || !amount.length || !payment_date.length || !payment_release_date.length || !payment_method.length || !payment_note.length ){
+
+            if (!vendor.length || !amount.length || !payment_date.length || !payment_release_date.length || !payment_method.length || !payment_note.length) {
                 alert("Please Fillup the Fields !");
                 return;
             }
-            
+
             $("#tf-report-loader").addClass('show');
-            
+
             jQuery.ajax({
                 type: 'post',
                 url: tf_options.ajax_url,
@@ -513,14 +544,14 @@
             })
         });
 
-        
+
         /*
         * Author @Jahid
         * Payout Update
         */
 
         $(document).on('click', '.tf-payout-edit', function () {
-            var withdraw_id =$(this).attr("data-id");
+            var withdraw_id = $(this).attr("data-id");
             $("#tf-report-loader").addClass('show');
             jQuery.ajax({
                 type: 'post',
@@ -545,14 +576,14 @@
             var payment_method = $("#tf-uvendor-payment-method").val();
             var payment_note = $("#tf-upayment-note").val();
             var payment_id = $("#tf-payment-id").val();
-           
-            if( !vendor.length || !amount.length || !payment_date.length || !payment_release_date.length || !payment_method.length || !payment_note.length ){
+
+            if (!vendor.length || !amount.length || !payment_date.length || !payment_release_date.length || !payment_method.length || !payment_note.length) {
                 alert("Please Fillup the Fields !");
                 return;
             }
-            
+
             $("#tf-report-loader").addClass('show');
-            
+
             jQuery.ajax({
                 type: 'post',
                 url: tf_options.ajax_url,
