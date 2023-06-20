bec24591435e42a8206a83633ba863e00ca7eb1e
Ashik384 <39790492+Ashik384@users.noreply.github.com>
2023-06-20 17:43:32 +0600

    = 2.9.20 -  20 June 2023 =
    * Added: Hotel single page new template
    * Added: Hotel Archive page new template
    * Added: Hotel Search page new template
    * Added: Tour single page new template
    * Added: Tour Archive page new template
    * Added: Tour Search page new template
    * Added: Tour Inventory 
    * Added: Dynamic pricing in tour extra

--- tourfic.php
+++ tourfic.php
@@ -7,11 +7,11 @@
  * Author URI:      https://themefic.com
  * Text Domain:     tourfic
  * Domain Path:     /lang/
- * Version:         2.9.19
+ * Version:         2.9.20
  * Tested up to:    6.2.2
- * WC tested up to: 7.7.2
+ * WC tested up to: 7.8.0
  * Requires PHP:    7.2
- * Elementor tested up to: 3.13.4
+ * Elementor tested up to: 3.14.0
  */
 
 // don't load directly
@@ -59,7 +59,7 @@ if(!class_exists('Appsero\Client')){
  * @since 1.0
  */
 if ( ! defined( 'TOURFIC' ) ) {
-	define( 'TOURFIC', '2.9.19' );
+	define( 'TOURFIC', '2.9.20' );
 }
 
 /**
