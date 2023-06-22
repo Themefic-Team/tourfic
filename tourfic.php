2aef9ab9a7624d4ad9c47fa3535e138bdc0bfc0a
Ashik384 <39790492+Ashik384@users.noreply.github.com>
2023-06-22 12:28:55 +0600

    = 2.9.21 -  22 June 2023 =
    * Removed: Default Data 
    * Improved: Design

--- tourfic.php
+++ tourfic.php
@@ -7,7 +7,7 @@
  * Author URI:      https://themefic.com
  * Text Domain:     tourfic
  * Domain Path:     /lang/
- * Version:         2.9.20
+ * Version:         2.9.21
  * Tested up to:    6.2.2
  * WC tested up to: 7.8.0
  * Requires PHP:    7.2
@@ -59,7 +59,7 @@ if(!class_exists('Appsero\Client')){
  * @since 1.0
  */
 if ( ! defined( 'TOURFIC' ) ) {
-	define( 'TOURFIC', '2.9.20' );
+	define( 'TOURFIC', '2.9.21' );
 }
 
 /**
