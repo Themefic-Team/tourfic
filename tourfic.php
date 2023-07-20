3c46d7179ffcb3e99e778c0dae1d55b869084e6e
Ashik384 <39790492+Ashik384@users.noreply.github.com>
2023-07-20 15:10:58 +0600

    = 2.9.22 -  20 July 2023 =
    = 2.9.22 -  20 July 2023 =
    
    * Added: Openstreetmap added to frontend
    * Added: New template section dynamic
    * Fixed: Body Google Fonts Issue
    * Fixed: Per Day booking price

--- tourfic.php
+++ tourfic.php
@@ -7,11 +7,11 @@
  * Author URI:      https://themefic.com
  * Text Domain:     tourfic
  * Domain Path:     /lang/
- * Version:         2.9.21
+ * Version:         2.9.22
  * Tested up to:    6.2.2
- * WC tested up to: 7.8.0
+ * WC tested up to: 7.9.0
  * Requires PHP:    7.2
- * Elementor tested up to: 3.14.0
+ * Elementor tested up to: 3.14.1
  */
 
 // don't load directly
@@ -59,7 +59,7 @@ if(!class_exists('Appsero\Client')){
  * @since 1.0
  */
 if ( ! defined( 'TOURFIC' ) ) {
-	define( 'TOURFIC', '2.9.21' );
+	define( 'TOURFIC', '2.9.22' );
 }
 
 /**
