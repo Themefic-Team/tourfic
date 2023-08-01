4adaa4c269844586013dd4571d6ef8e8a78e93da
Abu <abuhena.ict@gmail.com>
2023-08-01 18:24:16 +0600

    gallery, itnerary, update issue fixed

--- sass/admin/js/pro/admin.js
+++ sass/admin/js/pro/admin.js
@@ -107,7 +107,6 @@
                 action: "tf_export_tours",
                 nonce: tf_pro_params.nonce,
             },
-            dataType: 'json',
             beforeSend: function(){
                $('.tf-export-tours-btn').html('Exporting...');
             },
@@ -171,9 +170,10 @@
      */
     $(document).on('click', '.tf_import_tours_btn', function(e){
         e.preventDefault();
-        let formData          = $('#tf-import-tours').serializeArray();
-        let tour_csv_file_url = $('#tf-import-tours').find('input[name="tour_csv_file_url"]').val();
-        let import_csv_nonce  = $('#tf-import-tours').find('input[name="import_csv_nonce"]').val();
+        let formData                        = $('#tf-import-tours').serializeArray();
+        let tour_csv_file_url               = $('#tf-import-tours').find('input[name="tour_csv_file_url"]').val();
+        let import_csv_nonce                = $('#tf-import-tours').find('input[name="import_csv_nonce"]').val();
+        let tf_import_tours_update_existing = $('#tf-import-tours').find('input[name="tf_import_tours_update_existing"]').val();
         $('.tf-column-mapping-form').hide();
         
         $.ajax({
@@ -184,6 +184,7 @@
                 form_data: formData,
                 tour_csv_file_url: tour_csv_file_url,
                 import_csv_nonce: import_csv_nonce,
+                tf_import_tours_update_existing: tf_import_tours_update_existing,
             },
             beforeSend: function(){
                 $('.tf-step-1').addClass('active');
@@ -192,20 +193,7 @@
                 $('.tf-importing-progressbar-container').show();
             },
             success: function(response){
-                //get the percentage value from response
-                if( response.success ){
-                    console.log(response);
-                    let percentage = response.data.imported_percentage;
-                    console.log(percentage);
-                    $('.tf-importing-progressbar-container').show();
-                    $('.tf-importing-progressbar').css('width', percentage + '%');
-
-                    if( percentage == 100 ){
-                        $('.tf-importing-progressbar-container').hide();
-                        $('.tf_import_tours_btn').html('Import');
-                        $('.tf-step-3').addClass('active');
-                    }
-                }
+               
             },
             complete: function(){
                 $('.tf_import_tours_btn').html('Import');
@@ -223,9 +211,10 @@
      */
     $(document).on('click', '.tf_import_hotels_btn', function(e){
         e.preventDefault();
-        let formData           = $('#tf-import-hotels').serializeArray();
-        let hotel_csv_file_url = $('#tf-import-hotels').find('input[name="hotel_csv_file_url"]').val();
-        let import_csv_nonce   = $('#tf-import-hotels').find('input[name="import_csv_nonce"]').val();
+        let formData                         = $('#tf-import-hotels').serializeArray();
+        let hotel_csv_file_url               = $('#tf-import-hotels').find('input[name="hotel_csv_file_url"]').val();
+        let import_csv_nonce                 = $('#tf-import-hotels').find('input[name="import_csv_nonce"]').val();
+        let tf_import_hotels_update_existing = $('#tf-import-hotels').find('input[name="tf_import_hotels_update_existing"]').val();
         $('.tf-column-mapping-form').hide();
         
         $.ajax({
@@ -236,6 +225,7 @@
                 form_data: formData,
                 hotel_csv_file_url: hotel_csv_file_url,
                 import_csv_nonce: import_csv_nonce,
+                tf_import_hotels_update_existing: tf_import_hotels_update_existing,
             },
             beforeSend: function(){
                 $('.tf-step-1').addClass('done');
