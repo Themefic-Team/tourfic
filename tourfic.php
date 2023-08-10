8d5d76ba419f38b9fc664ac0cca80c00b8613d14
Ashik384 <39790492+Ashik384@users.noreply.github.com>
2023-08-10 16:27:28 +0600

    = 2.9.25 -  10 August 2023 =
    = 2.9.25 -  10 August 2023 =
    
    - Compatibility check with WordPress 6.3
    - Compatibility check with WooCommerce 8.0.1
    - Compatibility check with Elementor 3.15.2

--- tourfic.php
+++ tourfic.php
@@ -7,11 +7,11 @@
  * Author URI:      https://themefic.com
  * Text Domain:     tourfic
  * Domain Path:     /lang/
- * Version:         2.9.24
- * Tested up to:    6.2.2
- * WC tested up to: 7.9.0
+ * Version:         2.9.25
+ * Tested up to:    6.3
+ * WC tested up to: 8.0.1
  * Requires PHP:    7.2
- * Elementor tested up to: 3.15.1
+ * Elementor tested up to: 3.15.2
  */
 
 // don't load directly
@@ -59,7 +59,7 @@ if(!class_exists('Appsero\Client')){
  * @since 1.0
  */
 if ( ! defined( 'TOURFIC' ) ) {
-	define( 'TOURFIC', '2.9.24' );
+	define( 'TOURFIC', '2.9.22' );
 }
 
 /**
