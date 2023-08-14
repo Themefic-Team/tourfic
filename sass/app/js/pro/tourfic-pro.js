9bc2bdc9caf9f2b7d4787707652eb53c929a6fdb
mehedifoysal <mehedifoysal8@gmail.com>
2023-08-14 11:50:01 +0600

    login redirect uncomment

--- sass/app/js/pro/tourfic-pro.js
+++ sass/app/js/pro/tourfic-pro.js
@@ -638,7 +638,7 @@
                         form.find('textarea').closest('.tf-reg-field').find('small.text-danger').remove();
                     }
                     if (obj.redirect_url) {
-                        //window.location.href = obj.redirect_url;
+                        window.location.href = obj.redirect_url;
                     }
                     btn.removeClass('tf-btn-loading');
                 },
