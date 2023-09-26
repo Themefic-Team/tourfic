49fb3bb4a813ec5e344ef172738ea8830a6419cf
Ashik384 <39790492+Ashik384@users.noreply.github.com>
2023-09-26 11:39:47 +0600

    = 2.10.1 -  26 September 2023 =
    = 2.10.1 -  26 September 2023 =
    
    - Update: readme.txt

--- tourfic.php
+++ tourfic.php
@@ -7,7 +7,7 @@
  * Author URI:      https://themefic.com
  * Text Domain:     tourfic
  * Domain Path:     /lang/
- * Version:         2.10.0
+ * Version:         2.10.1
  * Tested up to:    6.3
  * WC tested up to: 8.1.1
  * Requires PHP:    7.2
@@ -59,7 +59,7 @@ if ( ! class_exists( 'Appsero\Client' ) ) {
  * @since 1.0
  */
 if ( ! defined( 'TOURFIC' ) ) {
-	define( 'TOURFIC', '2.10.0' );
+	define( 'TOURFIC', '2.10.1' );
 }
 
 /**
