cb80930810f7b6a5eb2f8f6f5112344ddedd07b7
mainulsunvi <mainulsunvi@gmail.com>
2023-10-10 14:57:12 +0600

    add nearby places for design default

--- templates/template-parts/hotel/design-1/places.php
+++ templates/template-parts/hotel/design-1/places.php
@@ -7,12 +7,12 @@ $places_meta = !empty($meta["nearby-places"]) ? $meta["nearby-places"] : array()
 <div class="tf-hotel-single-places  tf-mb-50 tf-template-section">
     <h2 class="tf-title tf-section-title"><?php echo __($places_section_title ,'tourfic') ?></h2>
     <ul>
-    <?php foreach ( $places_meta as $feature ) {
-         $feature_icon = '<i class="' . $feature['place-icon'] . '"></i>';
+    <?php foreach ( $places_meta as $place ) {
+         $place_icon = '<i class="' . $place['place-icon'] . '"></i>';
          ?>
          <li>
-            <span> <?php echo $feature_icon; ?> <?php echo $feature["place-title"] ?></span> 
-            <span> <?php echo $feature["place-dist"] ?></span>
+            <span> <?php echo $place_icon; ?> <?php echo $place["place-title"] ?></span> 
+            <span> <?php echo $place["place-dist"] ?></span>
         </li>
         <?php } ;?>
     </ul>
