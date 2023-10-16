cf6f482c90fbabe61cfae9ae50617a9fd2ea3601
jahid-dev <jahidhasancmt7@gmail.com>
2023-10-16 18:07:44 +0600

    popular facilites

--- templates/template-parts/hotel/design-2.php
+++ templates/template-parts/hotel/design-2.php
@@ -171,48 +171,22 @@
                     <?php the_content(); ?>
                 </div>
                 <div class="tf-overview-popular-facilities">
-                    <h3>Popular facilities</h3>
+                    <h3><?php _e("Popular facilities", "tourfic"); ?></h3>
                     <ul>
+                    <?php foreach ( $features as $feature ) {
+                        $feature_meta = get_term_meta( $feature->term_taxonomy_id, 'tf_hotel_feature', true );
+                        $f_icon_type  = ! empty( $feature_meta['icon-type'] ) ? $feature_meta['icon-type'] : '';
+                        if ( $f_icon_type == 'fa' && !empty($feature_meta['icon-fa']) ) {
+                            $feature_icon = '<i class="' . $feature_meta['icon-fa'] . '"></i>';
+                        } elseif ( $f_icon_type == 'c' && !empty($feature_meta['icon-c']) ) {
+                            $feature_icon = '<img src="' . $feature_meta['icon-c'] . '" style="width: ' . $feature_meta['dimention'] . 'px; height: ' . $feature_meta['dimention'] . 'px;" />';
+                        } ?>
+
                         <li>
-                            <i class="fa-solid fa-water-ladder"></i>
-                            <span>Swimming pool </span>
+                            <?php echo $feature_icon ?? ''; ?>
+                            <?php echo $feature->name; ?>
                         </li>
-                        <li>
-                            <i class="fa-solid fa-person-praying"></i>
-                            <span>Prayer zone</span>
-                        </li>
-                        <li>
-                            <i class="fa-solid fa-water-ladder"></i>
-                            <span>Swimming pool</span>
-                        </li>
-                        <li>
-                            <i class="fa-solid fa-person-praying"></i>
-                            <span>Prayer zone</span>
-                        </li>
-                        <li>
-                            <i class="fa-solid fa-water-ladder"></i>
-                            <span>Swimming pool</span>
-                        </li>
-                        <li>
-                            <i class="fa-solid fa-person-praying"></i>
-                            <span>Prayer zone</span>
-                        </li>
-                        <li>
-                            <i class="fa-solid fa-water-ladder"></i>
-                            <span>Swimming pool</span>
-                        </li>
-                        <li>
-                            <i class="fa-solid fa-person-praying"></i>
-                            <span>Prayer zone</span>
-                        </li>
-                        <li>
-                            <i class="fa-solid fa-water-ladder"></i>
-                            <span>Swimming pool</span>
-                        </li>
-                        <li>
-                            <i class="fa-solid fa-person-praying"></i>
-                            <span>Prayer zone</span>
-                        </li>
+                    <?php } ?>
                     </ul>
                 </div>
             </div>
