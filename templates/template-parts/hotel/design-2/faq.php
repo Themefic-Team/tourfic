5804349d5eb3c0f518627c1cc8611245b0b7b097
jahid-dev <jahidhasancmt7@gmail.com>
2023-10-18 18:23:15 +0600

    faq section design

--- templates/template-parts/hotel/design-2/faq.php
+++ templates/template-parts/hotel/design-2/faq.php
@@ -5,7 +5,7 @@
     <?php echo !empty($meta['faq-section-title']) ? esc_html($meta['faq-section-title']) : __( "Faq’s", 'tourfic' ); ?>
     </h2>            
     <div class="tf-questions">
-        <div class="tf-questions-col">
+        <!-- <div class="tf-questions-col">
             <?php 
             $faq_key = 1;    
             foreach ( $faqs as $key => $faq ): ?>
@@ -20,8 +20,43 @@
             </div>
             <?php $faq_key++; endforeach; ?>
             
-        </div>
+        </div> -->
         
+        <?php 
+        $faqs_itemsPerColumn = ceil(count($faqs) / 2);
+
+        ?>
+        <div class="tf-questions-col">
+            <?php 
+            for ($i = 0; $i < $faqs_itemsPerColumn; $i++) { ?>
+            <div class="tf-question <?php echo $i==0 ? esc_attr( 'tf-active' ) : ''; ?>">
+                <div class="tf-faq-head">
+                    <h3><?php echo esc_html( $faqs[$i]['title'] ); ?>
+                    <i class="fa-solid fa-chevron-down"></i></h3>
+                </div>
+                <div class="tf-question-desc" style="<?php echo $i==0 ? esc_attr( 'display: block;' ) : ''; ?>">
+                <?php echo wp_kses_post( $faqs[$i]['description'] ); ?>
+                </div>
+            </div>
+            <?php } ?>
+            
+        </div>
+        <div class="tf-questions-col">
+            <?php 
+            for ($i = $faqs_itemsPerColumn; $i < count($faqs); $i++) { ?>
+            <div class="tf-question">
+                <div class="tf-faq-head">
+                    <h3><?php echo esc_html( $faqs[$i]['title'] ); ?>
+                    <i class="fa-solid fa-chevron-down"></i></h3>
+                </div>
+                <div class="tf-question-desc">
+                <?php echo wp_kses_post( $faqs[$i]['description'] ); ?>
+                </div>
+                <i class="fa-solid fa-chevron-down"></i>
+            </div>
+            <?php } ?>
+            
+        </div>
     </div>
 </div>
 
