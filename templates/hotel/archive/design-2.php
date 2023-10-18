4f8d0874b2cc7631d5fddd559d22ac13dd1b03f2
jahid-dev <jahidhasancmt7@gmail.com>
2023-10-18 17:39:36 +0600

    sidebar design added

--- templates/hotel/archive/design-2.php
+++ templates/hotel/archive/design-2.php
@@ -39,36 +39,39 @@
                                 </label>
                             </div>
                             <div class="tf-booking-form-checkin">
-                                <span class="tf-booking-form-title">Check in</span>
+                                <span class="tf-booking-form-title"><?php _e("Check in", "tourfic"); ?></span>
                                 <div class="tf-booking-date-wrap">
-                                    <span class="tf-booking-date">00</span>
+                                    <span class="tf-booking-date"><?php _e("00", "tourfic"); ?></span>
                                     <span class="tf-booking-month">
-                                        <span>Mon</span>
-                                        <img src="./assets/image/select-arrow-dark.svg" alt="">
+                                        <span><?php _e("Month", "tourfic"); ?></span>
+                                        <img src="<?php echo TF_ASSETS_APP_URL ?>images/select-arrow-dark.svg" alt="">
                                     </span>
                                 </div>
-                                <input id="tf_checkin_date" type="text">
+                                <div class="tf_booking-dates">
+                                    <div class="tf_label-row"></div>
+                                </div>
                             </div>
                             <div class="tf-booking-form-checkout">
-                                <span class="tf-booking-form-title">Check out</span>
+                                <span class="tf-booking-form-title"><?php _e("Check out", "tourfic"); ?></span>
                                 <div class="tf-booking-date-wrap">
-                                    <span class="tf-booking-date">00</span>
+                                    <span class="tf-booking-date"><?php _e("00", "tourfic"); ?></span>
                                     <span class="tf-booking-month">
-                                        <span>Mon</span>
-                                        <img src="./assets/image/select-arrow-dark.svg" alt="">
+                                        <span><?php _e("Month", "tourfic"); ?></span>
+                                        <img src="<?php echo TF_ASSETS_APP_URL ?>images/select-arrow-dark.svg" alt="">
                                     </span>
                                 </div>
-                                <input id="tf_checkout_date" type="text">
+                                <input type="text" name="check-in-out-date" class="tf-check-in-out-date" onkeypress="return false;" placeholder="<?php _e( 'Select Date', 'tourfic' ); ?>" <?php echo ! empty( $check_in_out ) ? 'value="' . $check_in_out . '"' : '' ?> required>
+
                             </div>
                             <div class="tf-booking-form-guest-and-room">
                                 <div class="tf-booking-form-guest-and-room-inner">
-                                    <span class="tf-booking-form-title">Guests & rooms</span>
+                                    <span class="tf-booking-form-title"><?php _e("Guests", "tourfic"); ?></span>
                                     <div class="tf-booking-guest-and-room-wrap">
-                                        <span class="tf-guest">01</span> guest <span class="tf-room">01</span> Rooms
-                                    </div>
-                                    <div class="tf-booking-person-count">
-                                        <span>3 adults 1 children</span>
-                                        <img src="./assets/image/select-arrow-dark.svg" alt="">
+                                        <span class="tf-guest tf-booking-date"><?php _e("01", "tourfic"); ?></span> 
+                                        <span class="tf-booking-month">
+                                            <span><?php _e("Guest", "tourfic"); ?></span>
+                                            <img src="<?php echo TF_ASSETS_APP_URL ?>images/select-arrow-dark.svg" alt="">
+                                        </span>
                                     </div>
                                 </div>
 
@@ -76,26 +79,18 @@
                                 <div class="tf_acrselection-wrap">
                                     <div class="tf_acrselection-inner">
                                         <div class="tf_acrselection">
-                                            <div class="acr-label">Adults</div>
+                                            <div class="acr-label"><?php _e("Adults", "tourfic"); ?></div>
                                             <div class="acr-select">
                                                 <div class="acr-dec">-</div>
-                                                <input type="number" name="adults" id="adults" min="1" value="1">
+                                                <input type="number" name="adults" id="adults" min="1" value="<?php echo ! empty( $adults ) ? $adults : '1'; ?>">
                                                 <div class="acr-inc">+</div>
                                             </div>
                                         </div>
                                         <div class="tf_acrselection">
-                                            <div class="acr-label">Children</div>
+                                            <div class="acr-label"><?php _e("Children", "tourfic"); ?></div>
                                             <div class="acr-select">
                                                 <div class="acr-dec">-</div>
-                                                <input type="number" name="children" id="children" min="0" value="0">
-                                                <div class="acr-inc">+</div>
-                                            </div>
-                                        </div>
-                                        <div class="tf_acrselection">
-                                            <div class="acr-label">Rooms</div>
-                                            <div class="acr-select">
-                                                <div class="acr-dec">-</div>
-                                                <input type="number" name="room" id="room" min="1" value="1">
+                                                <input type="number" name="children" id="children" min="0" value="<?php echo ! empty( $child ) ? $child : '0'; ?>">
                                                 <div class="acr-inc">+</div>
                                             </div>
                                         </div>
@@ -164,35 +159,7 @@
                                 <input type="text">
                             </label>
                         </div>  
-                        <div class="tf-filter-list-item">                                
-                            <h4>Price range</h4>
-                            <div class="tf-filter-pricing-range"></div>
-                        </div>  
-                        <div class="tf-filter-list-item">
-                            <h4>Popular filter for Bangkok</h4>
-                            <ul>
-                                <li>
-                                    <input type="checkbox" name="" id="Breakfast">
-                                    <label for="Breakfast">Breakfast</label>                                        
-                                </li>
-                                <li>
-                                    <input type="checkbox" name="" id="Pool">
-                                    <label for="Pool">Pool</label>                                        
-                                </li>
-                                <li>
-                                    <input type="checkbox" name="" id="Hotel">
-                                    <label for="Hotel">Hotel</label>                                        
-                                </li>
-                                <li>
-                                    <input type="checkbox" name="" id="condition">
-                                    <label for="condition">Air condition</label>                                        
-                                </li>
-                                <li>
-                                    <input type="checkbox" name="" id="cancelation">
-                                    <label for="cancelation">Free cancelation</label>                                        
-                                </li>
-                            </ul>
-                        </div>
+                        
                         <div class="tf-filter-list-item">
                             <h4>Payment type</h4>
                             <ul>
@@ -210,137 +177,13 @@
                                 </li>
                             </ul>
                         </div>                            
-                        <div class="tf-filter-list-item">
-                            <h4>Meals</h4>
-                            <ul>
-                                <li>
-                                    <input type="checkbox" name="" id="">
-                                    <label for="asd">Fully refundable</label>                                        
-                                </li>
-                                <li>
-                                    <input type="checkbox" name="" id="">
-                                    <label for="asd">Breakfast included</label>                                        
-                                </li>
-                                <li>
-                                    <input type="checkbox" name="" id="">
-                                    <label for="asd">Free cancelation</label>                                        
-                                </li>
-                            </ul>
+                        
+                        <?php if ( is_active_sidebar( 'tf_archive_booking_sidebar' ) ) { ?>
+                        <div id="tf__booking_sidebar">
+                            <?php dynamic_sidebar( 'tf_archive_booking_sidebar' ); ?>
                         </div>
-                        <div class="tf-filter-list-item tf-filter-rating">
-                            <h4>rating</h4>
-                            <ul>
-                                <li>                                   
-                                    <input type="checkbox" name="" id="">
-                                    <label>                                                                     
-                                        <i class="fa-solid fa-star"></i>
-                                        <i class="fa-solid fa-star"></i>
-                                        <i class="fa-solid fa-star"></i>
-                                        <i class="fa-solid fa-star"></i>
-                                        <i class="fa-solid fa-star"></i>
-                                    </label>
-                                </li>
-                                <li>                                    
-                                    <input type="checkbox" name="" id="">
-                                    <label>                                       
-                                        <i class="fa-solid fa-star"></i>
-                                        <i class="fa-solid fa-star"></i>
-                                        <i class="fa-solid fa-star"></i>
-                                        <i class="fa-solid fa-star"></i>
-                                    </label>
-                                </li>
-                                <li>                                   
-                                    <input type="checkbox" name="" id="">
-                                    <label>                                         
-                                        <i class="fa-solid fa-star"></i>
-                                        <i class="fa-solid fa-star"></i>
-                                        <i class="fa-solid fa-star"></i>
-                                    </label>
-                                </li>
-                                <li>                                   
-                                    <input type="checkbox" name="" id="">
-                                    <label>                                         
-                                        <i class="fa-solid fa-star"></i>
-                                        <i class="fa-solid fa-star"></i>
-                                    </label>
-                                </li>
-                                <li>                                   
-                                    <input type="checkbox" name="" id="">
-                                    <label>                                      
-                                        <i class="fa-solid fa-star"></i>
-                                    </label>
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="tf-filter-list-item">
-                            <h4>Facilities</h4>
-                            <ul>
-                                <li>
-                                    <input type="checkbox" name="" id="">
-                                    <label for="asd">Breakfast</label>                                        
-                                </li>
-                                <li>
-                                    <input type="checkbox" name="" id="">
-                                    <label for="asd">Fireplace</label>                                        
-                                </li>
-                                <li>
-                                    <input type="checkbox" name="" id="">
-                                    <label for="asd">Non-smoking</label>                                        
-                                </li>
-                                <li>
-                                    <input type="checkbox" name="" id="">
-                                    <label for="asd">Breakfast</label>                                        
-                                </li>
-                                <li>
-                                    <input type="checkbox" name="" id="">
-                                    <label for="asd">Fireplace</label>                                        
-                                </li>
-                                <li>
-                                    <input type="checkbox" name="" id="">
-                                    <label for="asd">Non-smoking</label>                                        
-                                </li>
-                                <li>
-                                    <input type="checkbox" name="" id="">
-                                    <label for="asd">Breakfast</label>                                        
-                                </li>
-                                <li>
-                                    <input type="checkbox" name="" id="">
-                                    <label for="asd">Fireplace</label>                                        
-                                </li>
-                                <li>
-                                    <input type="checkbox" name="" id="">
-                                    <label for="asd">Non-smoking</label>                                        
-                                </li>
-                                <li>
-                                    <a href="" class="tf-filter-list-see-less">See less</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="tf-filter-list-item">
-                            <h4>Bed type</h4>
-                            <ul>
-                                <li>
-                                    <input type="checkbox" name="" id="">
-                                    <label for="asd">Twin bed</label>                                        
-                                </li>
-                                <li>
-                                    <input type="checkbox" name="" id="">
-                                    <label for="asd">Double bed</label>                                        
-                                </li>
-                                <li>
-                                    <input type="checkbox" name="" id="">
-                                    <label for="asd">King bed</label>                                        
-                                </li>
-                                <li>
-                                    <input type="checkbox" name="" id="">
-                                    <label for="asd">Single bed</label>                                        
-                                </li>
-                                <li>
-                                    <input type="checkbox" name="" id="">
-                                    <label for="asd">Fireplace</label>                                        
-                                </li>
-                            </ul>
-                        </div>    
+                        <?php } ?>
+        
                     </div> 
 
                     
