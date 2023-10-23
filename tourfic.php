6789ff836994e8d294242a014908a68b42e0499d
Ashik384 <39790492+Ashik384@users.noreply.github.com>
2023-10-23 17:48:06 +0600

    = 2.10.2 -  23 October 2023 =
    = 2.10.2 -  23 October 2023 =
    
    - Added: iCal integration for Tourfic hotels with booking.com(Pro).
    - Added: Discount feature for hotel rooms.
    - Added: The ability to set minimum and maximum stay duration for hotels.
    - Fixed: Partial payment popup label and description for tours.
    - Added: Global and custom settings option for Tour Itinerary Builder.
    - Added: The option to hide contact information for vendors’ front end and itinerary.
    - Added: Automatic location suggestions on all Search Pages
    - Fixed: Selected related tour issues.
    - Revamped: Totel, tour, and apartment booking details.
    - Revamped: The hotel room availability repeater to a calendar(Pro).

--- tourfic.php
+++ tourfic.php
@@ -7,11 +7,11 @@
  * Author URI:      https://themefic.com
  * Text Domain:     tourfic
  * Domain Path:     /lang/
- * Version:         2.10.1
+ * Version:         2.10.2
  * Tested up to:    6.3
- * WC tested up to: 8.1.1
+ * WC tested up to: 8.2.1
  * Requires PHP:    7.2
- * Elementor tested up to: 3.16.4
+ * Elementor tested up to: 3.16.6
  */
 
 // don't load directly
@@ -59,7 +59,7 @@ if ( ! class_exists( 'Appsero\Client' ) ) {
  * @since 1.0
  */
 if ( ! defined( 'TOURFIC' ) ) {
-	define( 'TOURFIC', '2.10.1' );
+	define( 'TOURFIC', '2.10.2' );
 }
 
 /**
