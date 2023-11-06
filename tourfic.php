e458bf12d6f060d9b722c5d8729418545845a07b
Ashik384 <39790492+Ashik384@users.noreply.github.com>
2023-11-06 21:05:27 +0600

    = 2.10.5 - 06 November 2023 =
    = 2.10.5 - 06 November 2023 =
    
    - Improvement: Settings Panel Label Updated
    - Fixed: Discount issue 
    - Fixed: Booking details font family issue for Mac
    - Improvement: Hotel room inventory moved to free
    - Improvement: Review filed moved to free
    - Improvement: Optimization moved to free

--- tourfic.php
+++ tourfic.php
@@ -7,7 +7,7 @@
  * Author URI:      https://themefic.com
  * Text Domain:     tourfic
  * Domain Path:     /lang/
- * Version:         2.10.4
+ * Version:         2.10.5
  * Tested up to:    6.3
  * WC tested up to: 8.2.1
  * Requires PHP:    7.2
@@ -59,7 +59,7 @@ if ( ! class_exists( 'Appsero\Client' ) ) {
  * @since 1.0
  */
 if ( ! defined( 'TOURFIC' ) ) {
-	define( 'TOURFIC', '2.10.4' );
+	define( 'TOURFIC', '2.10.5' );
 }
 
 /**
