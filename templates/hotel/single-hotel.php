2d6b25747696264d932ad123d89ec0f746b6e0f2
Abu <abuhena.ict@gmail.com>
2023-11-08 15:36:14 +0600

    Working on Hotel export

--- templates/hotel/single-hotel.php
+++ templates/hotel/single-hotel.php
@@ -28,6 +28,8 @@ while ( have_posts() ) : the_post();
 	 * Get hotel meta values
 	 */
 	$meta = get_post_meta( $post_id, 'tf_hotels_opt', true );
+    echo "<pre>";
+    print_r($meta);
 
 	$disable_share_opt  = ! empty( $meta['h-share'] ) ? $meta['h-share'] : '';
 	$disable_review_sec = ! empty( $meta['h-review'] ) ? $meta['h-review'] : '';
