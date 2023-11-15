ef5fb3c1cd19d128b58912c5c4b3ba45ebea29c9
jahid-dev <jahidhasancmt7@gmail.com>
2023-11-15 15:03:38 +0600

    time dealy added

--- inc/functions/functions-helper.php
+++ inc/functions/functions-helper.php
@@ -38,9 +38,17 @@ function tf_documentation_page_integration() {
  */
 if(!function_exists('tf_black_friday_2023_admin_notice') && !is_plugin_active( 'tourfic-pro/tourfic-pro.php' )){
     function tf_black_friday_2023_admin_notice(){
+
+		// Set the expiration time to 3 hours from the current time
+		$expiration_time = time() + 3 * 60 * 60;  
+		$tf_display_admin_notice_time = get_option( 'tf_display_admin_notice_time' );
+		if($tf_display_admin_notice_time == ''){
+			update_option( 'tf_display_admin_notice_time', $expiration_time );
+		}
+
         $deal_link =sanitize_url('https://themefic.com/deals/');
         $get_current_screen = get_current_screen();  
-        if(!isset($_COOKIE['tf_dismiss_admin_notice']) && $get_current_screen->base == 'dashboard'){ 
+        if(!isset($_COOKIE['tf_dismiss_admin_notice']) && $get_current_screen->base == 'dashboard' && time() > $tf_display_admin_notice_time){ 
             ?>
             <style> 
                 .tf_black_friday_20222_admin_notice a:focus {
@@ -96,7 +104,8 @@ if(!function_exists('tf_black_friday_notice_dismiss_callback')){
     function tf_black_friday_notice_dismiss_callback() { 
         $cookie_name = "tf_dismiss_admin_notice";
         $cookie_value = "1"; 
-        setcookie($cookie_name, $cookie_value, strtotime('2023-12-01'), "/"); 
+        setcookie($cookie_name, $cookie_value, strtotime('2023-12-01'), "/");
+		update_option( 'tf_display_admin_notice_time', '1' );
         wp_die();
     }
     add_action( 'wp_ajax_tf_black_friday_notice_dismiss_callback', 'tf_black_friday_notice_dismiss_callback' );
