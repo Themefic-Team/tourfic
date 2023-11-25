38b351132e1b2a8161b5c1e9ed694dd1ed55eb1e
Ashik384 <39790492+Ashik384@users.noreply.github.com>
2023-11-25 19:46:48 +0600

    = 2.10.10 - 25 November 2023 =
    = 2.10.10 - 25 November 2023 =
    
    - Improved: Icon field Ajax infinite scroll
    - Fixed: Performance issue fixed

--- tourfic.php
+++ tourfic.php
@@ -7,7 +7,7 @@
  * Author URI:      https://themefic.com
  * Text Domain:     tourfic
  * Domain Path:     /lang/
- * Version:         2.10.9
+ * Version:         2.10.10
  * Tested up to:    6.4
  * WC tested up to: 8.3
  * Requires PHP:    7.2
@@ -59,7 +59,7 @@ if ( ! class_exists( 'Appsero\Client' ) ) {
  * @since 1.0
  */
 if ( ! defined( 'TOURFIC' ) ) {
-	define( 'TOURFIC', '2.10.9' );
+	define( 'TOURFIC', '2.10.10' );
 }
 
 /**
