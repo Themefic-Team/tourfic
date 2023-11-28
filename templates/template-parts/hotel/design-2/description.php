b229a8ba0366aeee6a7b34a9f44d12b471507745
jahid-dev <jahidhasancmt7@gmail.com>
2023-11-28 18:21:28 +0600

    descriptio limit

--- templates/template-parts/hotel/design-2/description.php
+++ templates/template-parts/hotel/design-2/description.php
@@ -1,7 +1,13 @@
 <!--Overview Start -->
 <div class="tf-overview-wrapper">
     <div class="tf-overview-description">
-        <?php the_content(); ?>
+        <?php 
+        if(strlen(get_the_content()) > 300 ){
+            echo tourfic_character_limit_callback(get_the_content(), 300);
+        }else{
+            the_content(); 
+        }
+        ?>
     </div>
 </div>
 <!--Overview End -->
