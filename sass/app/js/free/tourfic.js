629f116ed9b52b84feb15d4dabed2e668f0bc88f
jahid-dev <jahidhasancmt7@gmail.com>
2024-01-28 18:27:31 +0600

    features slider

--- sass/app/js/free/tourfic.js
+++ sass/app/js/free/tourfic.js
@@ -2070,6 +2070,48 @@
                 }
             });
         });
+
+        /**
+         * Apartment 2 design Highlights sliers
+         *
+         * Slick
+         */
+        $('.tf-features-block-slides').slick({
+            dots: false,
+            arrows: false,
+            infinite: true,
+            speed: 300,
+            autoplay: true,
+            autoplaySpeed: 2000,
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 4,
+                        slidesToScroll: 1,
+                        infinite: true,
+                        dots: true
+                    }
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 1
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1
+                    }
+                }
+            ]
+        });
+
     });
 
     /*
--- templates/template-parts/apartment/design-1/features.php
+++ templates/template-parts/apartment/design-1/features.php
@@ -1,7 +1,9 @@
-<?php if ( isset( $meta['highlights'] ) && ! empty( tf_data_types( $meta['highlights'] ) ) ) : ?>
+<?php if ( isset( $meta['highlights'] ) && ! empty( tf_data_types( $meta['highlights'] ) ) ) : 
+   $tf_highlights_count = count(tf_data_types( $meta['highlights'] ));
+?>
 <!--Information Section Start -->
 <div class="tf-overview-wrapper">
-    <div class="tf-features-block-wrapper tf-informations-secations">
+    <div class="<?php echo $tf_highlights_count > 4 ? esc_attr('tf-features-block-slides') : esc_attr('tf-features-block-wrapper'); ?> tf-informations-secations">
         
         <?php
         foreach ( tf_data_types( $meta['highlights'] ) as $highlight ) :
