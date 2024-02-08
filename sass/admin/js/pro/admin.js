367ffdbe1a79a6dde907c873df8d08fcb222f6a0
jahid-dev <jahidhasancmt7@gmail.com>
2024-02-08 17:00:11 +0600

    apartment importing

--- sass/admin/js/pro/admin.js
+++ sass/admin/js/pro/admin.js
@@ -279,6 +279,47 @@
 
     });
 
+    /**
+     * Import Aprtments ajax
+     * @author Jahid
+     */
+    $(document).on('click', '.tf_import_apartments_btn', function(e){
+        e.preventDefault();
+        let formData                         = $('#tf-import-apartments').serializeArray();
+        let apartment_csv_file_url               = $('#tf-import-apartments').find('input[name="apartment_csv_file_url"]').val();
+        let import_csv_nonce                 = $('#tf-import-apartments').find('input[name="import_csv_nonce"]').val();
+        let tf_import_apartments_update_existing = $('#tf-import-apartments').find('input[name="tf_import_apartments_update_existing"]').val();
+        $('.tf-column-mapping-form').hide();
+        
+        $.ajax({
+            type: 'post',
+            url: ajaxurl,
+            data:{
+                action: 'tf_import_apartments',
+                form_data: formData,
+                apartment_csv_file_url: apartment_csv_file_url,
+                import_csv_nonce: import_csv_nonce,
+                tf_import_apartments_update_existing: tf_import_apartments_update_existing,
+            },
+            beforeSend: function(){
+                $('.tf-step-1').addClass('done');
+                $('.tf-step-2').addClass('done');
+                $('.tf-step-3').addClass('done');
+                $('.tf-importing-progressbar-container').show();
+            },
+            success: function(response){
+                
+            },
+            complete: function(){
+                $('.tf_import_apartments_btn').html('Import');
+                $('.tf-step-4').addClass('done');
+                $('.tf-importing-progressbar-container').hide();
+                $('.tf-import-complete-wrap').show();
+            },
+        });
+
+    });
+
     let urlParams = new URLSearchParams(window.location.search);
     let mapping   = urlParams.get('step');
     if( mapping == 'tour_mapping' ){
@@ -286,6 +327,10 @@
         $('.tf-step-2').addClass('active');
     }
     if( mapping == 'hotel_mapping' ){
+        $('.tf-step-1').addClass('done');
+        $('.tf-step-2').addClass('active');
+    }
+    if( mapping == 'apartment_mapping' ){
         $('.tf-step-1').addClass('done');
         $('.tf-step-2').addClass('active');
     }
