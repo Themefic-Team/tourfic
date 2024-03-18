f1c1fd70c20ffd68deb84ad3446afd25ddce8e46
Jahid Hasan <jahidhasancmt7@gmail.com>
2024-03-18 15:23:22 +0600

    favorite feature add to frontend

--- templates/template-parts/hotel/design-2/features.php
+++ templates/template-parts/hotel/design-2/features.php
@@ -4,8 +4,35 @@
         <span class="tf-popular-facilities-title"><?php esc_html_e("Popular facilities", "tourfic"); ?></span>
         <ul>
         <?php 
-        if(!empty($features)){
+        $favo_facilites_list = [];
+        if( !empty($meta['hotel-facilities']) ){
+            foreach( $meta['hotel-facilities'] as $facility ){
+                $favo_facilites_list [$facility['facilities-feature']] = $facility['facilities-feature'];
+            }
+        } 
+
+        if(!empty($favo_facilites_list)){
+        foreach ( $favo_facilites_list as $feature ) {
+        $feature_meta = get_term_meta( $feature, 'tf_hotel_feature', true );
+        $feature_name = get_term( $feature );
+        $f_icon_type  = ! empty( $feature_meta['icon-type'] ) ? $feature_meta['icon-type'] : '';
+        if ( $f_icon_type == 'fa' && !empty($feature_meta['icon-fa']) ) {
+            $feature_icon = '<i class="' . $feature_meta['icon-fa'] . '"></i>';
+        }
+        if ( $f_icon_type == 'c' && !empty($feature_meta['icon-c']) ) {
+            $feature_icon = '<img src="' . $feature_meta['icon-c'] . '" style="width: ' . $feature_meta['dimention'] . 'px; height: ' . $feature_meta['dimention'] . 'px;" />';
+        }
+        ?>
+        <li>
+                <?php echo !empty($feature_meta) && !empty($feature_icon) ? wp_kses_post($feature_icon) : ''; ?>
+                <?php echo !empty($feature_name->name) ? esc_html($feature_name->name) : ''; ?>
+            </li>
+        <?php } } ?>
+
+        <?php if( !empty($features) && count($favo_facilites_list) < 8 ){
+        $features_number = 1;
         foreach ( $features as $feature ) {
+            if( $features_number <= 8-count($favo_facilites_list) ){
             $feature_meta = get_term_meta( $feature->term_taxonomy_id, 'tf_hotel_feature', true );
             $f_icon_type  = ! empty( $feature_meta['icon-type'] ) ? $feature_meta['icon-type'] : '';
             if ( $f_icon_type == 'fa' && !empty($feature_meta['icon-fa']) ) {
@@ -18,9 +45,9 @@
 
             <li>
                 <?php echo !empty($feature_meta) && !empty($feature_icon) ? wp_kses_post($feature_icon) : ''; ?>
-                <?php echo esc_html($feature->name); ?>
+                <?php echo !empty($feature->name) ? esc_html($feature->name) : ''; ?>
             </li>
-        <?php } } ?>
+        <?php } $features_number++; } } ?>
         </ul>
     </div>
 </div>
