a251828fff65a6c9f38d35d5316c2e2521c7f359
mehedifoysal <mehedifoysal8@gmail.com>
2024-04-17 17:52:24 +0600

    plugin check issue fix

--- admin/tf-options/classes/TF_Settings.php
+++ admin/tf-options/classes/TF_Settings.php
@@ -34,29 +34,6 @@ if ( ! class_exists( 'TF_Settings' ) ) {
 
 			//ajax save options
 			add_action( 'wp_ajax_tf_options_save', array( $this, 'tf_ajax_save_options' ) );
-
-            add_action('init', array($this, 'tf_update_htaccess'));
-        }
-
-		function tf_update_htaccess() {
-
-            // Read the contents of .htaccess
-            $htaccess_contents = file_get_contents('.htaccess');
-
-            // Update max_input_vars if it exists, otherwise add it
-            if (preg_match('/^\s*php_value\s+max_input_vars\s+(\d+)\s*$/m', $htaccess_contents, $matches)) {
-                // If max_input_vars directive already exists, update its value
-                $max_input_vars = intval($matches[1]);
-                if ($max_input_vars < 3000) {
-                    $htaccess_contents = preg_replace('/^\s*php_value\s+max_input_vars\s+\d+\s*$/m', 'php_value max_input_vars 3000', $htaccess_contents);
-                }
-            } else {
-                // If max_input_vars directive doesn't exist, add it
-                $htaccess_contents .= "\nphp_value max_input_vars 3000\n";
-            }
-
-            // Write the updated contents back to .htaccess
-            file_put_contents('.htaccess', $htaccess_contents);
         }
 
         public static function option( $key, $params = array() ) {
@@ -693,7 +670,7 @@ if ( ! class_exists( 'TF_Settings' ) ) {
 			}
 
 
-            $ajax_save_class = 'tf-ajax-save';
+            $ajax_save_class = 'tf-ajax-savess';
 
 			if ( ! empty( $this->option_sections ) ) :
 				?>
--- admin/tf-options/metaboxes/tf-hotel-metabox.php
+++ admin/tf-options/metaboxes/tf-hotel-metabox.php
@@ -143,7 +143,8 @@ TF_Metabox::metabox( 'tf_hotels_opt', array(
 					'class'    => 'gmaps',
 					'type'     => 'map',
 					'label'    => esc_html__( 'Dynamic Location Search', 'tourfic' ),
-					'subtitle' => sprintf( wp_kses_post(__( 'Enter the specific address you wish to use for the hotel and select the correct option from the suggested addresses. This will be used to hyperlink address and display the address on the front-end map. <strong>Google Maps is also available for location. Simply set up your <a href="%s" target="_blank">Google Maps API Key</a></strong>', 'tourfic' )), esc_url( admin_url('admin.php?page=tf_settings#tab=map_settings') ) ),
+					/* translators: %s is the link to the Google Maps API Key settings */
+					'subtitle' => sprintf( wp_kses_post(esc_html__( 'Enter the specific address you wish to use for the hotel and select the correct option from the suggested addresses. This will be used to hyperlink address and display the address on the front-end map. <strong>Google Maps is also available for location. Simply set up your <a href="%s" target="_blank">Google Maps API Key</a></strong>', 'tourfic' )), esc_url( admin_url('admin.php?page=tf_settings#tab=map_settings') ) ),
 					'height'   => '250px',
 					'settings' => array(
 						'scrollWheelZoom' => true,
