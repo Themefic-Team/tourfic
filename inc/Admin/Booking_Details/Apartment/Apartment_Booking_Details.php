057f1325670d7ce3c24db6af315d152b87b61afe
mainulsunvi <mainulsunvi@gmail.com>
2024-05-20 17:57:41 +0600

    Backend booking revamped v2

---
+++ inc/Admin/Booking_Details/Apartment/Apartment_Booking_Details.php
@@ -0,0 +1,25 @@
+<?php
+namespace Tourfic\Admin\Booking_Details\Apartment;
+defined( 'ABSPATH' ) || exit;
+
+class Apartment_Booking_Details extends \Tourfic\Admin\Booking_Details\Booking_Details
+{
+    use \Tourfic\Traits\Singleton;
+
+    public function __construct()
+    {
+
+        $booking_args = array(
+            'post_type' => 'tf_apartment',
+            'menu_title' => esc_html__('Apartment Booking Details', 'tourfic'),
+            'menu_slug' => 'tf_apartment_booking',
+            'capability' => 'edit_tf_apartments',
+			'booking_type' => 'apartment',
+            'booking_title' => esc_html__('Apartment', 'tourfic'),
+        );
+
+        parent::__construct($booking_args);
+
+    }
+
+}
