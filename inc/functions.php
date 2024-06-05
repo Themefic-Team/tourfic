f652ed6dd94b3741a602410daf5ac02577df2ac1
mainulsunvi <mainulsunvi@gmail.com>
2024-06-05 12:43:44 +0600

    TF_Review Revamped v3

--- inc/functions.php
+++ inc/functions.php
@@ -94,15 +94,6 @@ if ( Helper::tf_is_woo_active() ) {
 }
 
 /**
- * Review Functions
- */
-// if ( file_exists( TF_INC_PATH . 'functions/functions-review.php' ) ) {
-// 	require_once TF_INC_PATH . 'functions/functions-review.php';
-// } else {
-// 	tf_file_missing( TF_INC_PATH . 'functions/functions-review.php' );
-// }
-
-/**
  * Include export import function file
  */
 if( file_exists( TF_INC_PATH . 'functions/functions-settings-import-export.php' ) ){
