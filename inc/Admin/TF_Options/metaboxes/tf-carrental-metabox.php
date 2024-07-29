1c29dca8f4b963e3a9d00c4246dd5b5add9db4ef
jahid-dev <jahidhasancmt7@gmail.com>
2024-07-29 16:16:11 +0600

    rearrage the fields

--- inc/Admin/TF_Options/metaboxes/tf-carrental-metabox.php
+++ inc/Admin/TF_Options/metaboxes/tf-carrental-metabox.php
@@ -11,9 +11,48 @@ TF_Metabox::metabox( 'tf_carrental_opt', array(
 
 		// location
 		'location'        => array(
-			'title'  => esc_html__( 'Location', 'tourfic' ),
-			'icon'   => 'fa-solid fa-location-dot',
+			'title'  => esc_html__( 'General Settings', 'tourfic' ),
+			'icon'   => 'fa-solid fa-cog',
 			'fields' => array(
+				array(
+					'id'    => 'carrental-General-heading',
+					'type'  => 'heading',
+					'label' => 'General Information',
+				),
+				array(
+					'id'    => 'carrental_gallery',
+					'type'  => 'gallery',
+					'label' => esc_html__( 'Car Gallery', 'tourfic' ),
+					'subtitle' => esc_html__( 'Add multiple images to craft a captivating gallery for your car, giving potential customers a visual car.', 'tourfic' ),
+				),
+                array(
+					'id'       => 'tf_single_carrental_layout_opt',
+					'type'     => 'select',
+					'label'    => esc_html__( 'Single Car Template Settings', 'tourfic' ),
+					'subtitle' => esc_html__( 'You can keep the Global Template settings or choose a different layout for this car.', 'tourfic' ),
+					'options'  => [
+						'global' => esc_html__( 'Global Settings', 'tourfic' ),
+						'single' => esc_html__( 'Single Settings', 'tourfic' ),
+					],
+					'default'  => 'global',
+				),
+                array(
+					'id'       => 'tf_single_tour_template',
+					'type'     => 'imageselect',
+					'label'    => esc_html__( 'Single Car Page Layout', 'tourfic' ),
+					'multiple' 		=> true,
+					'inline'   		=> true,
+					'options'   	=> array( 
+						'design-1' 				=> array(
+							'title'			=> 'Design 1',
+							'url' 			=> TF_ASSETS_ADMIN_URL."images/template/design1-tour.jpg",
+						)
+					),
+					'default'   	=> 'design-1',
+					'dependency'  => [
+						array( 'tf_single_carrental_layout_opt', '==', 'single' )
+					],
+				),
 				array(
 					'id'    => 'carrental-location-heading',
 					'type'  => 'heading',
@@ -54,6 +93,240 @@ TF_Metabox::metabox( 'tf_carrental_opt', array(
 					'icon' => 'ri-information-fill',
 					'content' => wp_kses_post(__( 'We\'re offering an additional feature called <b>property surroundings</b> in our pro plan. <a href="https://tourfic.com/" target="_blank">Upgrade to our pro package today to take advantage of this fantastic option!</a>', 'tourfic' ) ),
 				),
+			),
+		),
+
+		// Car Details
+		'car_details'         => array(
+			'title'  => esc_html__( 'Car Details', 'tourfic' ),
+			'icon'   => 'fa-solid fa-calendar-check',
+			'fields' => array(
+				array(
+					'id'    => 'carrental-car-details-heading',
+					'type'  => 'heading',
+					'label' => 'Car Details',
+					'subtitle' => esc_html__( 'This section offer the options to customize the booking process for this car.', 'tourfic' ),
+				),
+				array(
+					'id'      => 'carrental-booking-docs',
+					'type'    => 'notice',
+					'style'   => 'success',
+					'content' => esc_html__( 'If anything is not clear, please', 'tourfic' ) . ' <a href="https://themefic.com/docs/tourfic/add-new-apartment/booking-settings/" target="_blank" class="tf-admin-btn tf-btn-secondary tf-small-btn"><strong>' . esc_html__( 'Check our Documentation', 'tourfic' ) . '</strong></a>',
+				),
+                array(
+					'id'       => 'carrental_as_featured',
+					'type'     => 'switch',
+					'label'    => esc_html__( 'Set car as featured', 'tourfic' ),
+					'subtitle' => esc_html__( 'This car will be featured at the top of both the search results and the car archive page.', 'tourfic' ),
+				),
+                
+				array(
+					'id'          => 'passengers',
+					'type'        => 'number',
+					'label'       => esc_html__( 'No. Passengers', 'tourfic' ),
+					'subtitle'    => esc_html__( 'No. Passengers', 'tourfic' ),
+					'attributes'  => array( 'min' => 1 )
+				),
+				array(
+					'id'          => 'baggage',
+					'type'        => 'number',
+					'label'       => esc_html__( 'Baggage', 'tourfic' ),
+					'subtitle'    => esc_html__( 'Baggage', 'tourfic' ),
+					'attributes'  => array( 'min' => 0 )
+				),
+				array(
+					'id'          => 'door',
+					'type'        => 'number',
+					'label'       => __( 'No. Door', 'tourfic' ),
+					'subtitle'    => __( 'No. Door', 'tourfic' ),
+					'attributes'  => array( 'min' => 0 )
+				),
+				array(
+					'id'       => 'auto_transmission',
+					'type'     => 'switch',
+					'label'    => esc_html__( 'Auto Transmission', 'tourfic' ),
+					'subtitle' => esc_html__( 'Auto Transmission', 'tourfic' ),
+				),
+				array(
+					'id'       => 'unlimited_mileage',
+					'type'     => 'switch',
+					'label'    => esc_html__( 'Unlimited Mileage', 'tourfic' ),
+					'subtitle' => esc_html__( 'Unlimited Mileage', 'tourfic' ),
+				),
+				array(
+					'id'       => 'mileage_type',
+					'type'     => 'select',
+					'label'    => __( 'Type', 'tourfic' ),
+					'options'  => array(
+						'km' => __( 'Kilometer', 'tourfic' ),
+						'miles'   => __( 'Miles', 'tourfic' ),
+					),
+					'default'  => 'km',
+					'dependency'  => [
+						array( 'unlimited_mileage', '==', 'false' )
+					],
+					'field_width' => 50
+				),
+				array(
+					'id'    => 'mileage',
+					'type'  => 'text',
+					'label' => __( 'Mileage', 'tourfic' ),
+					'dependency'  => [
+						array( 'unlimited_mileage', '==', 'false' )
+					],
+					'field_width' => 50
+				),
+                array(
+					'id'       => 'pay_pickup',
+					'type'     => 'switch',
+					'label'    => esc_html__( 'Pay at Pick-up', 'tourfic' ),
+					'subtitle' => esc_html__( 'Pay at Pick-up', 'tourfic' ),
+				),
+				array(
+					'id'       => 'fuel_included',
+					'type'     => 'switch',
+					'label'    => esc_html__( 'Fuel Included', 'tourfic' ),
+					'subtitle' => esc_html__( 'Fuel Included', 'tourfic' ),
+				),
+				array(
+					'id'       => 'shuttle_car',
+					'type'     => 'switch',
+					'label'    => esc_html__( 'Shuttle to Car', 'tourfic' ),
+					'subtitle' => esc_html__( 'Shuttle to Car', 'tourfic' )
+				),
+				array(
+					'id'       => 'shuttle_car_fee_type',
+					'type'     => 'select',
+					'label'    => __( 'Price Type', 'tourfic' ),
+					'options'  => array(
+						'free' => __( 'Free', 'tourfic' ),
+						'paid'   => __( 'Paid', 'tourfic' ),
+					),
+					'default'  => 'free',
+					'dependency'  => [
+						array( 'shuttle_car', '==', 'true' )
+					],
+					'field_width' => 50
+				),
+				array(
+					'id'    => 'shuttle_car_fee',
+					'type'  => 'text',
+					'label' => __( 'Price', 'tourfic' ),
+					'dependency'  => [
+						array( 'shuttle_car_fee_type', '==', 'paid' )
+					],
+					'field_width' => 50
+				),
+				array(
+					'id'       => 'driver_included',
+					'type'     => 'switch',
+					'label'    => esc_html__( 'Driver included', 'tourfic' ),
+					'subtitle' => esc_html__( 'Driver included', 'tourfic' ),
+				),
+                array(
+					'id'    => 'carrental-driverinfo-heading',
+					'type'  => 'heading',
+					'label' => 'Driver Details Section',
+					'subtitle' => __( 'How can potential or existing customers reach out for more details about your car? Please share your Driver Details here.', 'tourfic' ),
+					'dependency'  => [
+						array( 'driver_included', '==', 'true' )
+					],
+				),
+				array(
+					'id'       => 'carrental_driverinfo_section',
+					'type'     => 'switch',
+					'label'    => esc_html__( 'Do you want to show driver information in the frontend?', 'tourfic' ),
+					'subtitle' => esc_html__( 'Do you want to show driver information in the frontend?', 'tourfic' ),
+					'dependency'  => [
+						array( 'driver_included', '==', 'true' )
+					],
+				),
+				array(
+					'id'          => 'driver_name',
+					'type'        => 'text',
+					'label'       => __( 'Name', 'tourfic' ),
+					'subtitle'       => __( 'This will be displayed in the Contact Section. Leave it blank if it is not necessary.', 'tourfic' ),
+					'field_width' => '50',
+					'dependency'  => [
+						array( 'driver_included', '==', 'true' )
+					],
+				),
+				array(
+					'id'          => 'driver_email',
+					'type'        => 'text',
+					'label'       => __( 'Email address', 'tourfic' ),
+					'subtitle'       => __( 'This will be displayed in the Contact Section. Leave it blank if it is not necessary.', 'tourfic' ),
+					'field_width' => '50',
+					'dependency'  => [
+						array( 'driver_included', '==', 'true' )
+					],
+				),
+				array(
+					'id'          => 'driver_phone',
+					'type'        => 'text',
+					'label'       => __( 'Phone Number', 'tourfic' ),
+					'subtitle'       => __( 'This will be displayed in the Contact Section. Leave it blank if it is not necessary.', 'tourfic' ),
+					'field_width' => '50',
+					'dependency'  => [
+						array( 'driver_included', '==', 'true' )
+					],
+				),
+				array(
+					'id'          => 'driver_age',
+					'type'        => 'number',
+					'label'       => __( 'Age', 'tourfic' ),
+					'subtitle'       => __( 'This will be displayed in the Contact Section. Leave it blank if it is not necessary.', 'tourfic' ),
+					'field_width' => '50',
+					'dependency'  => [
+						array( 'driver_included', '==', 'true' )
+					],
+				),
+				array(
+					'id'          => 'driver_address',
+					'type'        => 'text',
+					'label'       => __( 'Address', 'tourfic' ),
+					'subtitle'       => __( 'This will be displayed in the Contact Section. Leave it blank if it is not necessary.', 'tourfic' ),
+					'dependency'  => [
+						array( 'driver_included', '==', 'true' )
+					],
+				),
+				array(
+					'id'      => 'driver_image',
+					'type'    => 'image',
+					'label'   => __( 'Driver Photo', 'tourfic' ),
+					'subtitle'    => __( 'Please upload the driver photo to be displayed in the Contact Section.', 'tourfic' ),
+					'library' => 'image',
+					'dependency'  => [
+						array( 'driver_included', '==', 'true' )
+					],
+				),
+				array(
+					'id'    => 'carrental-custom-info-heading',
+					'type'  => 'heading',
+					'label' => 'Add Custom Informations',
+					'subtitle' => esc_html__( 'This section offer the options to customize the booking process for this car.', 'tourfic' ),
+				),
+				array(
+					'id'           => 'carrental_custom_info',
+					'type'         => 'repeater',
+					'button_title' => __( 'Add New Custom Info', 'tourfic' ),
+					'label'        => __( 'Add Your Custom Infos', 'tourfic' ),
+					'subtitle'        => __( 'Click the button below to add Info for your Car. Feel free to add as many as needed. Additionally, you can duplicate or rearrange each Badge using the icons on the right side.', 'tourfic' ),
+					'field_title'  => 'title',
+					'fields'       => array(
+						array(
+							'id'    => 'title',
+							'type'  => 'text',
+							'label' => __( 'Title', 'tourfic' ),
+						),
+                        array(
+                            'id'       => 'info_icon',
+                            'type'     => 'icon',
+                            'label'    => __( 'Icon', 'tourfic' ),
+                            'subtitle' => __( 'Choose icon', 'tourfic' ),
+                        )
+					),
+				)
 			),
 		),
 
@@ -103,128 +376,6 @@ TF_Metabox::metabox( 'tf_carrental_opt', array(
 			),
 		),
 
-        // Car Details
-		'car_details'         => array(
-			'title'  => esc_html__( 'Car Details', 'tourfic' ),
-			'icon'   => 'fa-solid fa-calendar-check',
-			'fields' => array(
-				array(
-					'id'    => 'carrental-booking-heading',
-					'type'  => 'heading',
-					'label' => 'Booking Settings',
-					'subtitle' => esc_html__( 'This section offer the options to customize the booking process for this car.', 'tourfic' ),
-				),
-				array(
-					'id'      => 'carrental-booking-docs',
-					'type'    => 'notice',
-					'style'   => 'success',
-					'content' => esc_html__( 'If anything is not clear, please', 'tourfic' ) . ' <a href="https://themefic.com/docs/tourfic/add-new-apartment/booking-settings/" target="_blank" class="tf-admin-btn tf-btn-secondary tf-small-btn"><strong>' . esc_html__( 'Check our Documentation', 'tourfic' ) . '</strong></a>',
-				),
-                array(
-					'id'       => 'carrental_as_featured',
-					'type'     => 'switch',
-					'label'    => esc_html__( 'Set car as featured', 'tourfic' ),
-					'subtitle' => esc_html__( 'This car will be featured at the top of both the search results and the car archive page.', 'tourfic' ),
-				),
-                array(
-					'id'    => 'carrental_gallery',
-					'type'  => 'gallery',
-					'label' => esc_html__( 'Car Gallery', 'tourfic' ),
-					'subtitle' => esc_html__( 'Add multiple images to craft a captivating gallery for your car, giving potential customers a visual car.', 'tourfic' ),
-				),
-                array(
-					'id'       => 'tf_single_carrental_layout_opt',
-					'type'     => 'select',
-					'label'    => esc_html__( 'Single Car Template Settings', 'tourfic' ),
-					'subtitle' => esc_html__( 'You can keep the Global Template settings or choose a different layout for this car.', 'tourfic' ),
-					'options'  => [
-						'global' => esc_html__( 'Global Settings', 'tourfic' ),
-						'single' => esc_html__( 'Single Settings', 'tourfic' ),
-					],
-					'default'  => 'global',
-				),
-                array(
-					'id'       => 'tf_single_tour_template',
-					'type'     => 'imageselect',
-					'label'    => esc_html__( 'Single Car Page Layout', 'tourfic' ),
-					'multiple' 		=> true,
-					'inline'   		=> true,
-					'options'   	=> array( 
-						'design-1' 				=> array(
-							'title'			=> 'Design 1',
-							'url' 			=> TF_ASSETS_ADMIN_URL."images/template/design1-tour.jpg",
-						)
-					),
-					'default'   	=> 'design-1',
-					'dependency'  => [
-						array( 'tf_single_carrental_layout_opt', '==', 'single' )
-					],
-				),
-				array(
-					'id'          => 'passengers',
-					'type'        => 'number',
-					'label'       => esc_html__( 'No. Passengers', 'tourfic' ),
-					'subtitle'    => esc_html__( 'No. Passengers', 'tourfic' ),
-					'field_width' => 33.33,
-					'attributes'  => array( 'min' => 1 )
-				),
-				array(
-					'id'          => 'baggage',
-					'type'        => 'number',
-					'label'       => esc_html__( 'Baggage', 'tourfic' ),
-					'subtitle'    => esc_html__( 'Baggage', 'tourfic' ),
-					'field_width' => 33.33,
-					'attributes'  => array( 'min' => 0 )
-				),
-				array(
-					'id'          => 'door',
-					'type'        => 'number',
-					'label'       => __( 'No. Door', 'tourfic' ),
-					'subtitle'    => __( 'No. Door', 'tourfic' ),
-					'field_width' => 33.33,
-					'attributes'  => array( 'min' => 0 )
-				),
-                array(
-					'id'       => 'pay_pickup',
-					'type'     => 'switch',
-					'label'    => esc_html__( 'Pay at Pick-up', 'tourfic' ),
-					'subtitle' => esc_html__( 'Pay at Pick-up', 'tourfic' ),
-				),
-				array(
-					'id'       => 'fuel_included',
-					'type'     => 'switch',
-					'label'    => esc_html__( 'Fuel Included', 'tourfic' ),
-					'subtitle' => esc_html__( 'Fuel Included', 'tourfic' ),
-				),
-				array(
-					'id'       => 'driver_included',
-					'type'     => 'switch',
-					'label'    => esc_html__( 'Driver included', 'tourfic' ),
-					'subtitle' => esc_html__( 'Driver included', 'tourfic' ),
-				),
-                array(
-					'id'       => 'unlimited_mileage',
-					'type'     => 'switch',
-					'label'    => esc_html__( 'Unlimited Mileage', 'tourfic' ),
-					'subtitle' => esc_html__( 'Unlimited Mileage', 'tourfic' ),
-				),
-				array(
-					'id'    => 'mileage',
-					'type'  => 'text',
-					'label' => __( 'Mileage', 'tourfic' ),
-					'dependency'  => [
-						array( 'unlimited_mileage', '==', 'false' )
-					],
-				),
-                array(
-					'id'       => 'shuttle_car',
-					'type'     => 'switch',
-					'label'    => esc_html__( 'Shuttle to Car', 'tourfic' ),
-					'subtitle' => esc_html__( 'Shuttle to Car', 'tourfic' )
-				)
-			),
-		),
-
         // Contact Information
 		'contact_info'         => array(
 			'title'  => __( 'Contact Information', 'tourfic' ),
@@ -270,53 +421,7 @@ TF_Metabox::metabox( 'tf_carrental_opt', array(
 					'subtitle'       => __( 'This will be displayed in the Contact Section. Leave it blank if it is not necessary.', 'tourfic' ),
 					'field_width' => '50',
 				),
-				array(
-					'id'    => 'carrental-driverinfo-heading',
-					'type'  => 'heading',
-					'label' => 'Driver Details Section',
-					'subtitle' => __( 'How can potential or existing customers reach out for more details about your car? Please share your Driver Details here.', 'tourfic' ),
-				),
-				array(
-					'id'          => 'driver_name',
-					'type'        => 'text',
-					'label'       => __( 'Name', 'tourfic' ),
-					'subtitle'       => __( 'This will be displayed in the Contact Section. Leave it blank if it is not necessary.', 'tourfic' ),
-					'field_width' => '50',
-				),
-				array(
-					'id'          => 'driver_email',
-					'type'        => 'text',
-					'label'       => __( 'Email address', 'tourfic' ),
-					'subtitle'       => __( 'This will be displayed in the Contact Section. Leave it blank if it is not necessary.', 'tourfic' ),
-					'field_width' => '50',
-				),
-				array(
-					'id'          => 'driver_phone',
-					'type'        => 'text',
-					'label'       => __( 'Phone Number', 'tourfic' ),
-					'subtitle'       => __( 'This will be displayed in the Contact Section. Leave it blank if it is not necessary.', 'tourfic' ),
-					'field_width' => '50',
-				),
-				array(
-					'id'          => 'driver_age',
-					'type'        => 'number',
-					'label'       => __( 'Age', 'tourfic' ),
-					'subtitle'       => __( 'This will be displayed in the Contact Section. Leave it blank if it is not necessary.', 'tourfic' ),
-					'field_width' => '50',
-				),
-				array(
-					'id'          => 'driver_address',
-					'type'        => 'text',
-					'label'       => __( 'Address', 'tourfic' ),
-					'subtitle'       => __( 'This will be displayed in the Contact Section. Leave it blank if it is not necessary.', 'tourfic' ),
-				),
-				array(
-					'id'      => 'driver_image',
-					'type'    => 'image',
-					'label'   => __( 'Driver Photo', 'tourfic' ),
-					'subtitle'    => __( 'Please upload the driver photo to be displayed in the Contact Section.', 'tourfic' ),
-					'library' => 'image',
-				),
+				
 			),
 		),
 
@@ -403,7 +508,6 @@ TF_Metabox::metabox( 'tf_carrental_opt', array(
 					'type'         => 'repeater',
 					'button_title' => __( 'Add New Price', 'tourfic' ),
 					'label'        => __( 'Price by date', 'tourfic' ),
-					'field_title'  => 'title',
 					'dependency' => array(
 						array( 'pricing_type', '==', 'date' ),
 					),
