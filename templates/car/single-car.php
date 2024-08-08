7ae03e99a3cb996a8c9e83ca08d3b561fa040c67
jahid-dev <jahidhasancmt7@gmail.com>
2024-08-08 16:28:43 +0600

    booking popup

--- templates/car/single-car.php
+++ templates/car/single-car.php
@@ -361,6 +361,136 @@ get_header();
                         
                     </div>
 
+                    <div class="tf-car-booking-popup">
+                        <div class="tf-booking-popup-warp">
+
+                            <div class="tf-booking-popup-header tf-flex tf-flex-align-center tf-flex-space-bttn">
+                                <h3>Additional information</h3>
+                                <div class="tf-close-popup">
+                                    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M15 5L5 15M5 5L15 15" stroke="#566676" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"/>
+                                    </svg>
+                                </div>
+                            </div>
+
+                            <div class="tf-booking-content-wraper">
+
+                                <div class="tf-cancellation-notice">
+                                    <span class="tf-flex tf-flex-align-center tf-flex-gap-16">
+                                        <i class="ri-information-line"></i>
+                                        <b>Free cancellation</b>
+                                        Full refund if you cancel your plan anytime before pick-up
+                                    </span>
+                                </div>
+
+                                <div class="tf-booking-tabs">
+                                    <ul>
+                                        <li class="active">Protections</li>
+                                        <li>Driver info</li>
+                                    </ul>
+                                </div>
+
+                                <div class="tf-protection-content">
+                                    <p>At the counter, the car hire company will block a deposit amount on your credit card. You could lose your whole deposit if the car is damaged or stolen, but as long as you have our Full Protection, Rentalcover.com will refund you! (The protection price you see includes all applicable taxes and fees).
+                                    </p>
+
+                                    <div class="tf-protection-featured">
+                                        <table>
+                                            <tr>
+                                                <td>What is covered</td>
+                                                <td align="center">No protection</td>
+                                                <td align="center">With protection</td>
+                                            </tr>
+
+                                            <tr>
+                                                <th>
+                                                    <div class="tf-flex">
+                                                        The car's excess
+                                                        <div class="tf-info-tooltip">
+                                                            <i class="ri-information-line"></i>
+                                                        </div>
+                                                    </div>
+                                                </th>
+                                                <td align="center">
+                                                    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                    <path d="M12.5001 7.49996L7.50008 12.5M7.50008 7.49996L12.5001 12.5M18.3334 9.99996C18.3334 14.6023 14.6025 18.3333 10.0001 18.3333C5.39771 18.3333 1.66675 14.6023 1.66675 9.99996C1.66675 5.39759 5.39771 1.66663 10.0001 1.66663C14.6025 1.66663 18.3334 5.39759 18.3334 9.99996Z" stroke="#566676" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"/>
+                                                    </svg>
+                                                </td>
+                                                <td align="center">
+                                                    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                    <path d="M18.3334 9.2333V9.99997C18.3324 11.797 17.7505 13.5455 16.6745 14.9848C15.5986 16.4241 14.0862 17.477 12.3629 17.9866C10.6396 18.4961 8.7978 18.4349 7.11214 17.8121C5.42648 17.1894 3.98729 16.0384 3.00922 14.5309C2.03114 13.0233 1.56657 11.24 1.68481 9.4469C1.80305 7.65377 2.49775 5.94691 3.66531 4.58086C4.83288 3.21482 6.41074 2.26279 8.16357 1.86676C9.91641 1.47073 11.7503 1.65192 13.3918 2.3833M7.50009 9.16664L10.0001 11.6666L18.3334 3.3333" stroke="#27BE69" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"/>
+                                                    </svg>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <th>
+                                                    <div class="tf-flex">
+                                                        The car's excess
+                                                        <div class="tf-info-tooltip">
+                                                            <i class="ri-information-line"></i>
+                                                        </div>
+                                                    </div>
+                                                </th>
+                                                <td align="center">
+                                                    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                    <path d="M12.5001 7.49996L7.50008 12.5M7.50008 7.49996L12.5001 12.5M18.3334 9.99996C18.3334 14.6023 14.6025 18.3333 10.0001 18.3333C5.39771 18.3333 1.66675 14.6023 1.66675 9.99996C1.66675 5.39759 5.39771 1.66663 10.0001 1.66663C14.6025 1.66663 18.3334 5.39759 18.3334 9.99996Z" stroke="#566676" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"/>
+                                                    </svg>
+                                                </td>
+                                                <td align="center">
+                                                    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                    <path d="M18.3334 9.2333V9.99997C18.3324 11.797 17.7505 13.5455 16.6745 14.9848C15.5986 16.4241 14.0862 17.477 12.3629 17.9866C10.6396 18.4961 8.7978 18.4349 7.11214 17.8121C5.42648 17.1894 3.98729 16.0384 3.00922 14.5309C2.03114 13.0233 1.56657 11.24 1.68481 9.4469C1.80305 7.65377 2.49775 5.94691 3.66531 4.58086C4.83288 3.21482 6.41074 2.26279 8.16357 1.86676C9.91641 1.47073 11.7503 1.65192 13.3918 2.3833M7.50009 9.16664L10.0001 11.6666L18.3334 3.3333" stroke="#27BE69" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"/>
+                                                    </svg>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <th>
+                                                    <div class="tf-flex">
+                                                        The car's excess
+                                                        <div class="tf-info-tooltip">
+                                                            <i class="ri-information-line"></i>
+                                                        </div>
+                                                    </div>
+                                                </th>
+                                                <td align="center">
+                                                    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                    <path d="M12.5001 7.49996L7.50008 12.5M7.50008 7.49996L12.5001 12.5M18.3334 9.99996C18.3334 14.6023 14.6025 18.3333 10.0001 18.3333C5.39771 18.3333 1.66675 14.6023 1.66675 9.99996C1.66675 5.39759 5.39771 1.66663 10.0001 1.66663C14.6025 1.66663 18.3334 5.39759 18.3334 9.99996Z" stroke="#566676" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"/>
+                                                    </svg>
+                                                </td>
+                                                <td align="center">
+                                                    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                    <path d="M18.3334 9.2333V9.99997C18.3324 11.797 17.7505 13.5455 16.6745 14.9848C15.5986 16.4241 14.0862 17.477 12.3629 17.9866C10.6396 18.4961 8.7978 18.4349 7.11214 17.8121C5.42648 17.1894 3.98729 16.0384 3.00922 14.5309C2.03114 13.0233 1.56657 11.24 1.68481 9.4469C1.80305 7.65377 2.49775 5.94691 3.66531 4.58086C4.83288 3.21482 6.41074 2.26279 8.16357 1.86676C9.91641 1.47073 11.7503 1.65192 13.3918 2.3833M7.50009 9.16664L10.0001 11.6666L18.3334 3.3333" stroke="#27BE69" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"/>
+                                                    </svg>
+                                                </td>
+                                            </tr>
+
+                                            <tfoot>
+                                                <tr>
+                                                    <th>Charge:</th>
+                                                    <th>$000</th>
+                                                    <th>$700</th>
+                                                </tr>
+                                            </tfoot>
+
+                                        </table>
+                                    </div>
+                                </div>
+
+                                <div class="tf-booking-bar">
+                                    <button class="without-charge">
+                                        Book without protection
+                                        <i class="ri-arrow-right-s-line"></i>
+                                    </button>
+                                    <button class="with-charge">
+                                        Book with protection
+                                        <i class="ri-arrow-right-s-line"></i>
+                                    </button>
+                                </div>
+
+                            </div>
+
+                        </div>
+                    </div>
+
                     <div class="tf-add-extra-section">
                         <h3>
                             Add extras
