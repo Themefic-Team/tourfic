296b210622e5bf4de8269a90d482ab842d5900ea
mehedifoysal <mehedifoysal8@gmail.com>
2024-10-06 23:49:27 +0600

    map popup scrollbar remove

--- inc/App/Widgets/TF_Widgets/Map_Filter.php
+++ inc/App/Widgets/TF_Widgets/Map_Filter.php
@@ -201,18 +201,6 @@ class Map_Filter extends \WP_Widget {
                                         <div class="tf-archive-top">
                                             <h5 class="tf-total-results"><?php _e("Found", "tourfic"); ?>
                                                 <span><?php echo $total_posts; ?></span> <?php _e("of", "tourfic"); ?> <?php echo $GLOBALS['wp_query']->found_posts; ?> <?php _e("Apartments", "tourfic"); ?></h5>
-                                            <a href="" class="tf-mobile-map-btn">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none">
-                                                    <path d="M17.3327 7.33366V6.68156C17.3327 5.06522 17.3327 4.25705 16.8445 3.75491C16.3564 3.25278 15.5707 3.25278 13.9993 3.25278H12.2671C11.5027 3.25278 11.4964 3.25129 10.8089 2.90728L8.03258 1.51794C6.87338 0.93786 6.29378 0.647818 5.67633 0.667975C5.05888 0.688132 4.49833 1.01539 3.37722 1.66992L2.354 2.2673C1.5305 2.74807 1.11876 2.98846 0.892386 3.38836C0.666016 3.78827 0.666016 4.27527 0.666016 5.24927V12.0968C0.666016 13.3765 0.666016 14.0164 0.951234 14.3725C1.14102 14.6095 1.40698 14.7688 1.70102 14.8216C2.1429 14.901 2.68392 14.5851 3.76591 13.9534C4.50065 13.5245 5.20777 13.079 6.08674 13.1998C6.82326 13.301 7.50768 13.7657 8.16602 14.0952"
-                                                          stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
-                                                    <path d="M5.66602 0.666992L5.66601 13.167" stroke="white" stroke-linejoin="round"/>
-                                                    <path d="M11.5 3.16699V6.91699" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
-                                                    <path d="M14.2556 17.0696C14.075 17.2388 13.8334 17.3333 13.5821 17.3333C13.3308 17.3333 13.0893 17.2388 12.9086 17.0696C11.254 15.5108 9.0366 13.7695 10.1179 11.2415C10.7026 9.87465 12.1061 9 13.5821 9C15.0581 9 16.4616 9.87465 17.0463 11.2415C18.1263 13.7664 15.9143 15.5162 14.2556 17.0696Z"
-                                                          stroke="white"/>
-                                                    <path d="M13.582 12.75H13.5895" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                                                </svg>
-                                                <span><?php echo esc_html__('Map', 'tourfic') ?></span>
-                                            </a>
                                             <ul class="tf-archive-view">
                                                 <li class="tf-archive-filter-btn">
                                                     <i class="ri-equalizer-line"></i>
@@ -377,17 +365,6 @@ class Map_Filter extends \WP_Widget {
                                 </div>
 
                                 <div class="tf-details-right tf-archive-right">
-                                    <a href="" class="tf-mobile-list-btn">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
-                                            <path d="M1.33398 7.59935C1.33398 6.82717 1.49514 6.66602 2.26732 6.66602H13.734C14.5062 6.66602 14.6673 6.82717 14.6673 7.59935V8.39935C14.6673 9.17153 14.5062 9.33268 13.734 9.33268H2.26732C1.49514 9.33268 1.33398 9.17153 1.33398 8.39935V7.59935Z"
-                                                  stroke="#FEF9F6" stroke-linecap="round"/>
-                                            <path d="M1.33398 2.26634C1.33398 1.49416 1.49514 1.33301 2.26732 1.33301H13.734C14.5062 1.33301 14.6673 1.49416 14.6673 2.26634V3.06634C14.6673 3.83852 14.5062 3.99967 13.734 3.99967H2.26732C1.49514 3.99967 1.33398 3.83852 1.33398 3.06634V2.26634Z"
-                                                  stroke="#FEF9F6" stroke-linecap="round"/>
-                                            <path d="M1.33398 12.9333C1.33398 12.1612 1.49514 12 2.26732 12H13.734C14.5062 12 14.6673 12.1612 14.6673 12.9333V13.7333C14.6673 14.5055 14.5062 14.6667 13.734 14.6667H2.26732C1.49514 14.6667 1.33398 14.5055 1.33398 13.7333V12.9333Z"
-                                                  stroke="#FEF9F6" stroke-linecap="round"/>
-                                        </svg>
-                                        <span><?php echo esc_html__('List view', 'tourfic') ?></span>
-                                    </a>
                                     <div id="map-marker" data-marker="<?php echo TF_ASSETS_URL . 'app/images/cluster-marker.png'; ?>"></div>
                                     <div class="tf-hotel-archive-map-wrap">
                                         <div id="tf-hotel-archive-map"></div>
