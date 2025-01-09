7184bc848e21c9ca46aae8d59545bf496b2bbde5
Jahid Hasan <jahidhasancmt7@gmail.com>
2025-01-09 11:43:31 +0600

    hotel divide

--- inc/Classes/Hotel/Hotel.php
+++ inc/Classes/Hotel/Hotel.php
@@ -4291,7 +4291,7 @@ class Hotel {
                             </script>
 
                         </div>
-                        <div class="tf-hotel-details-info" style="width:440px; padding-left: 35px;max-height: 470px;padding-top: 25px; overflow-y: scroll;">
+                        <div class="tf-hotel-details-info" style="width:440px; padding-left: 35px;max-height: 470px;padding-top: 25px; overflow-y: auto;">
 							<?php
 							$footage      = ! empty( $room['footage'] ) ? $room['footage'] : '';
 							$bed          = ! empty( $room['bed'] ) ? $room['bed'] : '';
