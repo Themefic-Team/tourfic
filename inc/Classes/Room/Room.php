63a508e46faf379f67f50fab7e29d5a09d462bb0
mainulsunvi <mainulsunvi@gmail.com>
2025-01-15 18:04:57 +0600

    get_hotel_rooms function update

--- inc/Classes/Room/Room.php
+++ inc/Classes/Room/Room.php
@@ -16,19 +16,29 @@ class Room {
 	}
 
 	static function get_hotel_rooms( $hotel_id ) {
-		$args = array(
-			'post_type'      => 'tf_room',
-			'posts_per_page' => - 1,
-		);
+		// $args = array(
+		// 	'post_type'      => 'tf_room',
+		// 	'posts_per_page' => - 1,
+		// );
 
-		$rooms = get_posts( $args );
+		// $rooms = get_posts( $args );
+
+		// $hotel_rooms = array();
+		// foreach ( $rooms as $room ) {
+		// 	$room_meta = get_post_meta( $room->ID, 'tf_room_opt', true );
+		// 	if ( ! empty( $room_meta['tf_hotel'] ) && $room_meta['tf_hotel'] == $hotel_id ) {
+		// 		$hotel_rooms[] = $room;
+		// 	}
+		// }
+
+		$hotel_meta = get_post_meta( $hotel_id, 'tf_hotels_opt', true );
+
+		$rooms = ! empty( $hotel_meta['tf_rooms'] ) ? $hotel_meta['tf_rooms'] : array();
+		asort($rooms);
 
 		$hotel_rooms = array();
 		foreach ( $rooms as $room ) {
-			$room_meta = get_post_meta( $room->ID, 'tf_room_opt', true );
-			if ( ! empty( $room_meta['tf_hotel'] ) && $room_meta['tf_hotel'] == $hotel_id ) {
-				$hotel_rooms[] = $room;
-			}
+			$hotel_rooms[] = get_post( $room );
 		}
 
 		return $hotel_rooms;
