15d2899b1dda97893c82da46efd0a3ddc2298c01
Mofazzal Hossain <mosharofmd74@gmail.com>
2025-02-13 18:49:53 +0600

    Fixed re-test issues

--- sass/app/js/free/car.js
+++ sass/app/js/free/car.js
@@ -203,6 +203,8 @@
         $(document).on('click', '.tf-booking-popup-header .tf-close-popup', function (e) {
             e.preventDefault();
             $('.tf-car-booking-popup').hide();
+            $('.tf-mobile-booking-btn').show();
+            $('.tf-mobile-booking-btn button').text('Book Now');
         });
 
         /*
@@ -253,6 +255,8 @@
                     $('.error-notice').hide();
                     $('.tf-car-booking-popup').css('display', 'flex');
                     $this.removeClass('tf-btn-loading');
+                    $(".tf-date-select-box").hide();
+                    $(".tf-mobile-booking-btn").hide();
                 }
             });
 
