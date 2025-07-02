b8662eb5badbb9e305c301391943116eb93b4819
Jahid Hasan <jahidhasancmt7@gmail.com>
2025-07-02 17:13:03 +0600

    related field attr add

--- inc/Admin/TF_Options/fields/number/TF_number.php
+++ inc/Admin/TF_Options/fields/number/TF_number.php
@@ -12,7 +12,27 @@ if ( ! class_exists( 'TF_number' ) ) {
 		public function render() {
 			$type = ( ! empty( $this->field['type'] ) ) ? $this->field['type'] : 'number';
 			$placeholder = ( ! empty( $this->field['placeholder'] ) ) ? 'placeholder="' . $this->field['placeholder'] . '"' : '';
-			echo '<input type="' . esc_attr( $type ) . '" name="' . esc_attr( $this->field_name() ) . '" id="' . esc_attr( $this->field_name() ) . '" value="' . esc_attr($this->value) . '" ' . wp_kses_post($placeholder) . ' '. wp_kses_post($this->field_attributes()) .'/>';
+			if($this->field['related']){ ?>
+				<div class="tf-unit-price-box">
+				<input type="<?php echo esc_attr( $type ); ?>" name="<?php echo esc_attr( $this->field_name() ); ?>" id="<?php echo esc_attr( $this->field_name() ); ?>" value="<?php echo esc_attr($this->value); ?>" <?php echo wp_kses_post($placeholder); ?><?php echo wp_kses_post($this->field_attributes()); ?> />
+				</div>
+			<?php 
+				echo '<select name="' . esc_attr($this->field['related_name']) . '" id="' . esc_attr( $this->field['related_name'] ) . '">';
+
+				if( isset($this->field['related_options']) && is_array( $this->field['related_options'] )) {
+					foreach ( $this->field['related_options'] as $key => $value ) {
+						if($key !== '') {
+							echo '<option value="' . esc_attr( $key ) . '" >' . esc_html( $value ) . '</option>';
+						} else {
+							//disable empty value
+							echo '<option value="" disabled>' . esc_html( $value ) . '</option>';
+						}
+					}
+				}
+				echo '</select>';
+			}else{
+				echo '<input type="' . esc_attr( $type ) . '" name="' . esc_attr( $this->field_name() ) . '" id="' . esc_attr( $this->field_name() ) . '" value="' . esc_attr($this->value) . '" ' . wp_kses_post($placeholder) . ' '. wp_kses_post($this->field_attributes()) .'/>';
+			}
 		}
 
 	}
