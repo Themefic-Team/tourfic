fc0f4bf40c1f511e44889549abf844f688900f58
Jahid Hasan <jahidhasancmt7@gmail.com>
2025-08-14 10:43:54 +0600

    security issue fixing

--- inc/Classes/Enqueue.php
+++ inc/Classes/Enqueue.php
@@ -1290,14 +1290,12 @@ class Enqueue {
 		$output = '';
 
 		if( wp_get_theme() && wp_get_theme()->get( 'TextDomain' ) === 'hello-elementor' ) {
-			$output .= <<<'EOD'
-			/* Fix for Hello Elementor theme flatpicker conflict */
-
+			$output .= '
 			.flatpickr-months .flatpickr-current-month {
 				display: flex;
 				gap: 8px;
 			}
-			EOD;
+			';
 		}
 
 		if (wp_style_is('tf-app-style', 'enqueued')) {
