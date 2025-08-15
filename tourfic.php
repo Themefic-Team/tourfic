de026cc522c72267ec315ecb454754c598e00456
Ashik384 <39790492+Ashik384@users.noreply.github.com>
2025-08-15 21:03:57 +0600

    gpl license updated

--- tourfic.php
+++ tourfic.php
@@ -12,7 +12,9 @@
  * WC tested up to: 10.0
  * Requires PHP:    7.4
  * Elementor tested up to: 3.31
- */
+ * License: GPLv2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+*/
 
 // Don't load directly
 defined( 'ABSPATH' ) || exit;
