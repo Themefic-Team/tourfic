526bb88e9b1f5bd3e1ce8a3c45b89ad7342c8ef2
Ashik384 <39790492+Ashik384@users.noreply.github.com>
2025-09-01 18:52:56 +0600

    = 2.16.15 – September 01, 2025 =
    = 2.16.15 – September 01, 2025 =
    
    - Addressed plugin review feedback.
    - Improved security and code quality.
    - Updated readme.txt info for OpenStreetMap.
    - Performance and stability improvements.

--- tourfic.php
+++ tourfic.php
@@ -7,7 +7,7 @@
  * Author URI:      https://themefic.com
  * Text Domain:     tourfic
  * Domain Path:     /lang/
- * Version:         2.16.14
+ * Version:         2.16.15
  * Tested up to:    6.8
  * WC tested up to: 10.0
  * Requires PHP:    7.4
@@ -28,7 +28,7 @@ final class Tourfic {
 	 * @var string
 	 */
 
-	const VERSION = '2.16.14';
+	const VERSION = '2.16.15';
 
 	/**
 	 * Minimum PHP version required.
